a5fcf53479bfa02d5840b573bc44a37dbeaa4fc8
Pegasus <martingda@protonmail.com>
2019-03-20 14:17:10 -0300

    Added user data structs

---
+++ processment/data/dataModel.go
@@ -0,0 +1,39 @@
+package data
+
+// UserData is the general struct for parsing data
+type UserData struct {
+	Data []UserTasks `json:"user-data"`
+}
+
+// UserTasks is the structure used for parsing all the tasks
+type UserTasks struct {
+	Task UserTask `json:"task"`
+}
+
+// UserTask is a struct for parsing every task
+type UserTask struct {
+	Trigger UserTrigger `json:"trigger"`
+	Actions []UserAction `json:"actions"` 
+}
+
+// UserTrigger is a struct for parsing every trigger
+type UserTrigger struct {
+	ID string `json:"ID"`
+	Args []UserArg `json:"args"`
+	Timestamp string `json:"timestamp"`
+}
+
+// UserAction is a struct for parsing every action
+type UserAction struct {
+	ID string `json:"ID"`
+	Args []UserArg `json:"args"`
+	Timestamp string `json:"timestamp"`
+	Order int `json:"order"`
+}
+
+// UserArg is a struct por arg parsing
+type UserArg struct {
+	ID string `json:"ID"`
+	Content string `json:"content"`
+}
+
