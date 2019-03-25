c268dc9438717e3176373d7f16f4f4efa8facbc9
Pegasus <martingda@protonmail.com>
2019-03-25 13:39:31 -0300

    Added multiple functions for user data update

--- processment/data/updateData.go
+++ processment/data/updateData.go
@@ -7,18 +7,16 @@ import (
 	"github.com/Pegasus8/piworker/utilities/files"
 )
 
-// UpdateData is a function used to update an existing task from the JSON data file
-func UpdateData(filename string, taskName string, 
-	updatedTask *UserTask) (bool, error) {
-	
-	data, err := ReadData(filename)
+// UpdateTask is a function used to update an existing task from the JSON data file.
+func UpdateTask(taskName string, updatedTask *UserTask) error {
+	data, err := ReadData()
 	if err != nil {
-		return false, err
+		return err
 	}
 
-	_, index, err := data.GetTask(taskName)
+	_, index, err := data.GetTaskByName(taskName)
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	log.Printf("Task with name '%s' finded, updating data...\n", taskName)
@@ -26,15 +24,147 @@ func UpdateData(filename string, taskName string,
 
 	byteData, err := json.MarshalIndent(data, "", "   ")
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	// Re-write data into file
-	_, err = files.WriteFile(".", filename, byteData)
+	_, err = files.WriteFile(".", Filename, byteData)
 	if err != nil {
-		return false, err
+		return err
 	}
 
 	log.Println("Task updated successfully")
-	return true, nil
+	return nil
+}
+
+// UpdateTaskName is a function used to change the name of a task.
+func UpdateTaskName(oldName, newName string) error {
+
+	data, err := ReadData()
+	if err != nil {
+		return err
+	}
+
+	oldTask, index, err := data.GetTaskByName(oldName)
+	if err != nil {
+		return err
+	}
+	
+	data.Tasks[index].Task = UserTask{
+		Name: newName,
+		State: oldTask.State,
+		Trigger: oldTask.Trigger,
+		Actions: oldTask.Actions,
+	}
+
+	byteData, err := json.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	_, err = files.WriteFile(".", Filename, byteData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateTaskTrigger is a function used to change the trigger of a task.
+func UpdateTaskTrigger(name string, newTrigger *UserTrigger) error {
+
+	data, err := ReadData()
+	if err != nil {
+		return err
+	}
+
+	oldTask, index, err := data.GetTaskByName(name)
+	if err != nil {
+		return err
+	}
+	
+	data.Tasks[index].Task = UserTask{
+		Name: oldTask.Name,
+		State: oldTask.State,
+		Trigger: *newTrigger,
+		Actions: oldTask.Actions,
+	}
+
+	byteData, err := json.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	_, err = files.WriteFile(".", Filename, byteData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateTaskActions is a function used to change the actions of a task.
+func UpdateTaskActions(name string, newActions *[]UserAction) error {
+
+	data, err := ReadData()
+	if err != nil {
+		return err
+	}
+
+	oldTask, index, err := data.GetTaskByName(name)
+	if err != nil {
+		return err
+	}
+	
+	data.Tasks[index].Task = UserTask{
+		Name: oldTask.Name,
+		State: oldTask.State,
+		Trigger: oldTask.Trigger,
+		Actions: *newActions,
+	}
+
+	byteData, err := json.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	_, err = files.WriteFile(".", Filename, byteData)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// UpdateTaskState is a function used to change the state of a task.
+func UpdateTaskState(name string, newState string) error {
+
+	data, err := ReadData()
+	if err != nil {
+		return err
+	}
+
+	oldTask, index, err := data.GetTaskByName(name)
+	if err != nil {
+		return err
+	}
+	
+	data.Tasks[index].Task = UserTask{
+		Name: oldTask.Name,
+		State: newState,
+		Trigger: oldTask.Trigger,
+		Actions: oldTask.Actions,
+	}
+
+	byteData, err := json.MarshalIndent(data, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	_, err = files.WriteFile(".", Filename, byteData)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
