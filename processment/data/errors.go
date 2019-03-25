17386f2c773bff6e2606000adb61cf866978388e
Pegasus <martingda@protonmail.com>
2019-03-25 13:34:27 -0300

    Added standard error  "ErrNoFilenameAsigned"

--- processment/data/errors.go
+++ processment/data/errors.go
@@ -5,9 +5,12 @@ import (
 )
 
 
-// ErrBadTaskName is an error used when a task with specific name is not finded
+// ErrBadTaskName is an error used when a task with specific name is not finded.
 // in the JSON data file.
 var	ErrBadTaskName = errors.New("Invalid task name: the task name provided not exists "+
 		"in the user database.")
 
+// ErrNoFilenameAsigned is an error used when the name of the json data file was not setted.
+var ErrNoFilenameAsigned = errors.New("No Filename: the filename of the data file was" + 
+	" not asigned")
 
