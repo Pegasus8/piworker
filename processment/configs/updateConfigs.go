0524f13466a0221348adfec1eec81d92026cd1d8
Pegasus <martingda@protonmail.com>
2019-03-29 13:25:17 -0300

    Implemented some functions to update existing configs

---
+++ processment/configs/updateConfigs.go
@@ -0,0 +1,96 @@
+package configs
+
+import (
+
+)
+
+// UpdateBehaviorConfigs is the function used to update the behavior configs in the
+// configs file.
+func UpdateBehaviorConfigs(behaviorCfg *Behavior) error {
+	data, err := ReadConfigs()
+	if err != nil {
+		return err
+	}
+
+	// Overwrite configs
+	data.Behavior = *behaviorCfg
+	
+	err = WriteConfigs(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// UpdateSecurityConfigs is the function used to update the security configs in the
+// configs file.
+func UpdateSecurityConfigs(securityCfg *Security) error {
+	data, err := ReadConfigs()
+	if err != nil {
+		return err
+	}
+
+	data.Security = *securityCfg
+
+	err = WriteConfigs(data)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// UpdateAPIConfigs is the function used to update the API configs in the
+// configs file.
+func UpdateAPIConfigs(apiCfg *APIConfigs) error {
+	data, err := ReadConfigs()
+	if err != nil {
+		return err
+	}
+
+	data.APIConfigs = *apiCfg
+
+	err = WriteConfigs(data)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// UpdateUpdatesConfigs is the function used to update the PiWorker updates configs in the
+// configs file.
+func UpdateUpdatesConfigs(updatesCfg *Updates) error {
+	data, err := ReadConfigs()
+	if err != nil {
+		return err
+	}
+
+	data.Updates = *updatesCfg
+
+	err = WriteConfigs(data)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// UpdateWebUIConfigs is the function used to update the WebUI configs in the
+// configs file.
+func UpdateWebUIConfigs(webuiCfg *WebUI) error {
+	data, err := ReadConfigs()
+	if err != nil {
+		return err
+	}
+
+	data.WebUI = *webuiCfg
+
+	err = WriteConfigs(data)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
