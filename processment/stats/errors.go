6e062544a54b6550b78a8e4e98f7ef7cc15ecfa8
Pegasus <martingda@protonmail.com>
2019-04-11 23:25:44 -0300

    Added standard error for bad parsing of Free Storage #6
    Aditionally, added comment for the error ErrBadCPULoadParse

--- processment/stats/errors.go
+++ processment/stats/errors.go
@@ -10,6 +10,12 @@ var ErrBadTemperatureParse = errors.New(
 	"Failed temperature parsing: can't locate the temperature",
 )
 
+// ErrBadCPULoadParse is the error used when the CPU load can't be parsed.
 var ErrBadCPULoadParse = errors.New(
 	"Falied CPU load parsing: can't locate the values",
+)
+
+// ErrBadFreeStorageParse is the error used when the Free Storage can't be parsed.
+var ErrBadFreeStorageParse = errors.New(
+	"Falied Free Storage parsing:  can't locate the value",
 )
