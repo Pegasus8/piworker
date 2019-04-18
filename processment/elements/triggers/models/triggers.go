21b170d86abb3992037904612fe0ad481fb66912
Pegasus <martingda@protonmail.com>
2019-04-18 10:06:05 -0300

    Moved triggers.go inside models package #13

---
+++ processment/elements/triggers/models/triggers.go
@@ -0,0 +1,10 @@
+package models
+
+import (
+	"github.com/Pegasus8/piworker/processment/elements/triggers"
+)
+
+// TRIGGERS is the main variable used to store all the triggers of PiWorker
+var TRIGGERS = []triggers.Trigger {
+	
+}
--- processment/elements/triggers/triggers.go
+++
@@ -1,6 +0,0 @@
-package triggers
-
-// TRIGGERS is the main variable used to store all the triggers of PiWorker
-var TRIGGERS = []Trigger {
-	
-}
