e343c8e7c2c47c05a612350f26dc62ad35cb7b44
Pegasus <martingda@protonmail.com>
2019-04-25 12:04:31 -0300

    Added some standard stats variables

---
+++ processment/stats/variables.go
@@ -0,0 +1,9 @@
+package stats
+
+const (
+	// DatabaseName is the name of the sqlite3 database used for storage of some statistics
+	DatabaseName = "stats.db"
+
+	// SatisticsPath is the path where the stats wanna be saved.
+	SatisticsPath = "./statistics/"	
+)
