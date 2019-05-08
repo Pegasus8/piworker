0e8c0ffb0061786b0a19f7b06fbd19969067ee86
Pegasus <martingda@protonmail.com>
2019-05-08 16:55:48 -0300

    Added action WriteTextFile into actions slice

--- processment/elements/actions/models/actions.go
+++ processment/elements/actions/models/actions.go
@@ -1,6 +1,10 @@
-package actions
+package models
+
+import  (
+	"github.com/Pegasus8/piworker/processment/elements/actions"
+)
 
 // ACTIONS is the main variable used to store all the actions of PiWorker
-var ACTIONS = []Action {
-	
+var ACTIONS = []actions.Action {
+	WriteTextFile,
 }
