10480acafb220eaa4dbb7e1e778f3659a03119f8
Pegasus <martingda@protonmail.com>
2019-05-13 10:36:09 -0300

    Typo

--- processment/elements/actions/models/errors.go
+++ processment/elements/actions/models/errors.go
@@ -9,7 +9,7 @@ var  (
 		"Error with the ID of an argument: unrecognized ID",
 	)
 
-	// ErrUnrecognizedWritingMode is the mode used when the mode inserted by the user 
+	// ErrUnrecognizedWritingMode is the error used when the mode inserted by the user 
 	// (on the action `WriteTextFile`) is out of the list of modes.
 	ErrUnrecognizedWritingMode = errors.New(
 		"Error with the writing mode: unrecognized mode",
