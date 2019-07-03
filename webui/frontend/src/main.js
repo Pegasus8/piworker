857aa78f4fc218a79d228d329886b710eb7d715c
Pegasus <martingda@protonmail.com>
2019-07-03 19:18:31 -0300

    Added baseURL for axios (only for APIs usage)

--- webui/frontend/src/main.js
+++ webui/frontend/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+                      // FIXME Check if https enabled on server
+axios.defaults.baseURL = `https://${location.host}/api`
 
 new Vue({
   router,
