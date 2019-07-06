0d06411254fde03d0499f8165d963059077348d8
Pegasus <martingda@protonmail.com>
2019-07-06 14:23:47 -0300

    Added "Boxes" component

---
+++ webui/frontend/src/views/Statistics.vue
@@ -0,0 +1,12 @@
+<template>
+    <app-boxes></app-boxes>
+</template>
+
+<script>
+import Boxes from '../components/statistics/Boxes.vue'
+export default {
+  components: {
+    appBoxes: Boxes
+  }
+}
+</script>
