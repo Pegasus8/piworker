b3b53a1362699c0ec861ac316c4b3061fb9f30b5
Pegasus <martingda@protonmail.com>
2019-07-06 14:25:12 -0300

    Added statistics view into routes's list

--- webui/frontend/src/router.js
+++ webui/frontend/src/router.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Statistics from './views/Statistics.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/statistics',
+      component: Statistics,
+      name: 'statistics',
+      beforeEnter: (to, from, next) => {
+        // Check for authentication
+        next()
+      }
+    }
+  ]
 })
