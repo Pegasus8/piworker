eeeec65438e9c3c68cae4d7eab00da2e39ed7053
Pegasus <martingda@protonmail.com>
2019-07-06 14:20:35 -0300

    Moved auth to a module

---
+++ webui/frontend/src/store/modules/auth.js
@@ -0,0 +1,98 @@
+import axios from 'axios'
+import router from '../../router'
+
+const state = {
+  tokenID: null,
+  userID: null,
+  user: null
+}
+
+const mutations = {
+  authUser: (state, userData) => {
+    state.tokenID = userData.token
+    state.userID = userData.userID
+  },
+  storeUser: (state, user) => {
+    state.user = user
+  },
+  clearAuthData: (state) => {
+    state.tokenID = null
+    state.userID = null
+    state.user = null
+  }
+}
+
+const actions = {
+  // `expirationTime` must be seconds
+  setLogoutTimer: ({ dispatch }, expirationTime) => {
+    setTimeout(() => {
+      dispatch('logout')
+    }, expirationTime * 1000)
+  },
+  logout: ({ commit }) => {
+    commit('clearAuthData')
+    localStorage.removeItem('token')
+    localStorage.removeItem('userID')
+    localStorage.removeItem('expirationTime')
+    router.replace('/signin') // TODO Add "/signin" path to routes's list
+  },
+  tryAutologin: ({ commit }) => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      return
+    }
+    // If the token was obtained, then we get all the info
+    const expirationTime = localStorage.getItem('expirationTime')
+    const now = new Date()
+    if (now >= expirationTime) {
+      // Token expired
+      return
+    }
+    // Token still valid
+    const userID = localStorage.getItem('userID')
+    commit('authUser', {
+      token,
+      userID
+    })
+  },
+  login: ({ commit, dispatch }, authData) => {
+    axios.post('/auth?key=<MASTER_KEY>', { // FIXME Replace for the user MASTER_KEY
+      user: authData.user,
+      password: authData.password
+    })
+      .then((response) => {
+        console.info('User logged, saving the info...')
+        //  Response: {token: "", userID: "", expiresIn: ""}  //
+
+        const now = new Date()
+        const expirationDate = new Date(now.getTime() + (response.data.expiresIn * 1000))
+        localStorage.setItem('token', response.data.token)
+        localStorage.setItem('userID', response.data.userID)
+        localStorage.setItem('expirationTime', expirationDate)
+
+        commit('authUser', {
+          tokenID: response.data.token,
+          userID: response.data.userID
+        })
+        dispatch('setLogoutTimer', response.data.expiresIn)
+      })
+      .catch((err) => console.error(err))
+  }
+}
+
+const getters = {
+  user: (state) => {
+    return state.user
+  },
+  isAuthenticated: (state) => {
+    return state.tokenID !== null
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
