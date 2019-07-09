128593a25a374799cd090a764f24f703981aef86
Pegasus <martingda@protonmail.com>
2019-07-09 17:08:32 -0300

    Added Panels component on statistics view

--- webui/frontend/src/views/Statistics.vue
+++ webui/frontend/src/views/Statistics.vue
@@ -1,12 +1,18 @@
 <template>
+  <div>
     <app-boxes></app-boxes>
+    <app-panels></app-panels>
+  </div>
 </template>
 
 <script>
 import Boxes from '../components/statistics/Boxes.vue'
+import Panels from '../components/statistics/Panels.vue'
+
 export default {
   components: {
-    appBoxes: Boxes
+    appBoxes: Boxes,
+    appPanels: Panels
   }
 }
 </script>
