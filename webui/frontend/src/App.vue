1a91fd753e24b885e4db52efd0934aa5cb656191
Pegasus <martingda@protonmail.com>
2019-07-12 18:12:29 -0300

    Added new task dialog

--- webui/frontend/src/App.vue
+++ webui/frontend/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <app-navbar></app-navbar>
+    <app-navbar/>
     <router-view/>
+    <app-new-task-modal/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/navbar/Navbar.vue'
+import NewTaskModal from './components/NewTask.vue'
 
 export default {
   components: {
-    appNavbar: Navbar
+    appNavbar: Navbar,
+    appNewTaskModal: NewTaskModal
   }
 }
 </script>
