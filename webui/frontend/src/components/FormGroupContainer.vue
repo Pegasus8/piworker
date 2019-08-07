fc59bf0db0be5f1f3aaf36e5fa95066c3be72e5f
Pegasus <martingda@protonmail.com>
2019-08-07 12:15:37 -0300

    Added component for new task dialog

---
+++ webui/frontend/src/components/FormGroupContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form-group rounded border p-3 my-4">
+    <label :for="topElementID" class="text-dark">{{ containerTitle }}</label>
+    <slot name="top"></slot>
+    <small :id='topElementID + "-description"' class="form-text text-muted text-left">{{ containerDescription }}</small>
+    <slot name="bottom"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        containerTitle: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        containerDescription: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        topElementID: {
+          type: String,
+          required: true,
+          default: ''
+        }
+    }
+}
+</script>
