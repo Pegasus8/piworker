c822bd830647a2f38e4db66d84a97702860d9778
Pegasus <martingda@protonmail.com>
2019-08-15 11:35:22 -0300

    Initialized management view

---
+++ webui/frontend/src/views/Management.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fluid-container p-4">
+      <!-- TODO This view has to be a preview of subviews -->
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
