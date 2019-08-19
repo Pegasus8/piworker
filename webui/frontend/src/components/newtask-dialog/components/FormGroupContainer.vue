92a399ef3e74e7f38586c3a536dc27b4db4180d9
Pegasus <martingda@protonmail.com>
2019-08-19 16:13:38 -0300

    Usage of card component from bootstrap

--- webui/frontend/src/components/newtask-dialog/components/FormGroupContainer.vue
+++ webui/frontend/src/components/newtask-dialog/components/FormGroupContainer.vue
@@ -1,30 +1,44 @@
 <template>
-  <div class="form-group rounded border p-3 my-4">
-    <label :for="topElementID" class="text-dark">{{ containerTitle }}</label>
-    <slot name="top"></slot>
-    <small :id='topElementID + "-description"' class="form-text text-muted text-left">{{ containerDescription }}</small>
-    <slot name="bottom"></slot>
+  <div class="form-group card p-0">
+    <div class="card-body">
+      <label :for="topElementID" class="card-title">{{ containerTitle }}</label>
+      <slot name="top"/>
+      <small :id='topElementID + "-description"' class="form-text text-muted text-left">{{ containerDescription }}</small>
+    </div>
+    <div v-if="withFooter" class="card-footer">
+      <slot name="bottom"/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
     props: {
-        containerTitle: {
-            type: String,
-            required: true,
-            default: ''
-        },
-        containerDescription: {
-            type: String,
-            required: true,
-            default: ''
-        },
-        topElementID: {
+      containerTitle: {
           type: String,
           required: true,
           default: ''
-        }
+      },
+      containerDescription: {
+          type: String,
+          required: true,
+          default: ''
+      },
+      topElementID: {
+        type: String,
+        required: true,
+        default: ''
+      },
+      withFooter: {
+        type: Boolean,
+        required: false,
+        default: false
+      }
+    },
+    data() {
+      return {
+        hasBottomSlot: false
+      }
     }
 }
 </script>
