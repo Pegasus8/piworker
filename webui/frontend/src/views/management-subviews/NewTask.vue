58bb5f09e482dfb1ee949953d5f420c68219fbdc
Pegasus <martingda@protonmail.com>
2019-08-19 16:07:08 -0300

    Regression - Deleted management subviews

--- webui/frontend/src/views/management-subviews/NewTask.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1 style="color: white;" class="mt-4">Hello from subview "New Task"</h1>
-</template>
--- webui/frontend/src/views/management-subviews/TasksList.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Hello from "TasksList" view!</h1>
-</template>
