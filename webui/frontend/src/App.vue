dd4f57e3b06f13094dfa0eb837132e4a3815331b
Pegasus <martingda@protonmail.com>
2019-09-10 10:53:30 -0300

    Import of icomoon css

--- webui/frontend/src/App.vue
+++ webui/frontend/src/App.vue
@@ -19,6 +19,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("assets/css/icomoon.css");
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
