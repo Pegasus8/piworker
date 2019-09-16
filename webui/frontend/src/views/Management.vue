d03faf6df85548c9585e82477962afa462449491
Pegasus <martingda@protonmail.com>
2019-09-16 10:17:52 -0300

    Added showing of user tasks

--- webui/frontend/src/views/Management.vue
+++ webui/frontend/src/views/Management.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="container p-4">
+  <div class="container p-4 text-center justify-content-center">
+    <h4 class="text-light">My Tasks</h4>
+    <div v-if="userTasks.length > 0" class="container-fluid">
+      <app-task
+        v-for="task in userTasks" :key="(task.Name).replace(/\s/g, '_')"
+        :taskName="task.Name"
+        :taskState="task.State"
+        :triggers="[task.Trigger]"
+        :actions="task.Actions"
+        logs="" 
+      />
+      <!-- TODO Logs integration -->
+    </div>
+    <div v-else class="alert alert-warning m-4" role="alert">
+      Oops... It seems that you have not created any task yet.
+      Let's click on the "New" button to create a new one!
+    </div>
   </div>
 </template>
 
@@ -8,10 +24,14 @@ import Task from '../components/management/Task.vue'
 export default {
   data() {
     return {
+      userTasks: []
     }
   },
   components: {
     appTask: Task
+  },
+  created () {
+    // TODO APIs requests
   }
 }
 </script>
