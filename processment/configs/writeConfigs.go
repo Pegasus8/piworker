c8f52ca8c02d9f86816f680910e3ff0fa6e6e2d9
Pegasus <martingda@protonmail.com>
2019-09-22 12:20:54 -0300

    Usage of default log package

--- processment/configs/writeConfigs.go
+++ processment/configs/writeConfigs.go
@@ -3,9 +3,10 @@ package configs
 
 import (
 	"encoding/json"
+	"log"
 
 	"github.com/Pegasus8/piworker/utilities/files"
-	"github.com/Pegasus8/piworker/utilities/log"
+	// "github.com/Pegasus8/piworker/utilities/log"
 )
 
 // WriteConfigs is a function used to write the configs into the configs file, 
@@ -13,21 +14,21 @@ import (
 func WriteConfigs(configs *Configs) error {
 	mutex.Lock()
 	defer mutex.Unlock()
-	log.Infoln("Writing configs...")
+	log.Println("Writing configs...")
 	
-	log.Infoln("Reading JSON data...")
+	log.Println("Reading JSON data...")
 	byteData, err := json.MarshalIndent(configs, "", "   ")
 	if err != nil {
 		return err
 	}
-	log.Infoln("JSON data read correctly")
+	log.Println("JSON data read correctly")
 
-	log.Infoln("Writing configs...")
+	log.Println("Writing configs...")
 	_, err = files.WriteFile(ConfigsPath, Filename, byteData)
 	if err != nil {
 		return err
 	}
-	log.Infoln("Configs writed successfully")
+	log.Println("Configs writed successfully")
 
 	return nil
 }
