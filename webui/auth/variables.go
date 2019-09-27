c187abec824304866251b229974943ee61a957ce
Pegasus <martingda@protonmail.com>
2019-09-27 11:28:06 -0300

    Added some global variables

---
+++ webui/auth/variables.go
@@ -0,0 +1,12 @@
+package auth
+
+import (
+	"github.com/Pegasus8/piworker/processment/data"
+)
+
+const (
+	// DatabaseName is the name of the sqlite3 database used for storage of auth info.
+	DatabaseName = "tokens.db"
+	// DatabasePath is the path where the auth info will be saved.
+	DatabasePath = data.DataPath
+)
