b268238a9cf4ac98da3050711b89b37c879717a0
Pegasus <martingda@protonmail.com>
2019-09-27 11:21:32 -0300

    typo

--- processment/stats/variables.go
+++ processment/stats/variables.go
@@ -4,6 +4,6 @@ const (
 	// DatabaseName is the name of the sqlite3 database used for storage of some statistics
 	DatabaseName = "stats.db"
 
-	// SatisticsPath is the path where the stats wanna be saved.
+	// SatisticsPath is the path where the stats will be saved.
 	SatisticsPath = "./statistics/"	
 )
