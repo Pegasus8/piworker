b534d2811ef6a8f2ee5fbf412ad2e211eeec3b12
Pegasus <martingda@protonmail.com>
2019-10-08 11:53:06 -0300

    Improved logging

--- webui/frontend/src/store/modules/auth.js
+++ webui/frontend/src/store/modules/auth.js
@@ -39,6 +39,7 @@ const actions = {
   tryAutologin: ({ commit }) => {
     const token = localStorage.getItem('token')
     if (!token) {
+      console.info("Can't found the token on the local storage, returning...")
       return
     }
     // If the token was obtained, then we get all the info
@@ -46,15 +47,21 @@ const actions = {
     const now = new Date()
     if (now >= expirationTime) {
       // Token expired
+      console.warn("Token expired, autologin canceled")
       return
     }
     // Token still valid
     const userID = localStorage.getItem('userID')
+    console.info("User found on the local storage, saving changes on vuex")
     commit('authUser', {
       token,
       userID
     })
+    console.info("Auth info commited on vuex")
+
+    console.info("Setting token header on axios")
     axios.defaults.headers.common['Token'] = response.data.token
+    console.info("Default axios header setted")
   },
   login: ({ commit, dispatch }, authData) => {
     axios.post('/api/login', { // FIXME Replace for the user MASTER_KEY
@@ -67,16 +74,27 @@ const actions = {
 
         const now = new Date()
         const expirationDate = new Date(now.getTime() + (response.data.expiresAt))
+        console.info("New expiration date:", expirationDate)
+
+        console.info("Saving auth info on local storage")
         localStorage.setItem('token', response.data.token)
         localStorage.setItem('userID', authData.user)
         localStorage.setItem('expirationTime', expirationDate)
+        console.info("Auth info saved on local storage")
 
+        console.info("Commiting auth info on vuex")
         commit('authUser', {
           tokenID: response.data.token,
           userID: authData.user
         })
+        console.info("Auth info commited, setting a logout timer...")
         dispatch('setLogoutTimer', response.data.expiresAt)
+        console.info("Logout timer setted");
+
+        console.info("Setting token header on axios")
         axios.defaults.headers.common['Token'] = response.data.token
+        console.info("Default axios header setted")
+        router.replace({ name: 'statistics'} )
       })
       .catch((err) => console.error(err))
   }
