58191a79a53c7ea9c305acd2bf0185cc5d4b0026
Pegasus <martingda@protonmail.com>
2019-10-08 10:35:44 -0300

    Conditional showing of nav items

--- webui/frontend/src/components/navbar/Navbar.vue
+++ webui/frontend/src/components/navbar/Navbar.vue
@@ -18,10 +18,10 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto text-center">
-        <router-link :to="{name: 'statistics'}" class="nav-item" active-class="active"><a class="nav-link">Statistics</a></router-link>
-        <router-link :to="{name: 'management'}" class="nav-item" active-class="active"><a class="nav-link">Management</a></router-link>
-        <router-link :to="{name: 'settings'}" class="nav-item" active-class="active"><a class="nav-link">Settings</a></router-link>
-        <li class="nav-item ml-lg-3">
+        <router-link v-if="$store.getters['auth/isAuthenticated']" :to="{name: 'statistics'}" class="nav-item" active-class="active"><a class="nav-link">Statistics</a></router-link>
+        <router-link v-if="$store.getters['auth/isAuthenticated']" :to="{name: 'management'}" class="nav-item" active-class="active"><a class="nav-link">Management</a></router-link>
+        <router-link v-if="$store.getters['auth/isAuthenticated']" :to="{name: 'settings'}" class="nav-item" active-class="active"><a class="nav-link">Settings</a></router-link>
+        <li v-if="$store.getters['auth/isAuthenticated']" class="nav-item ml-lg-3">
           <button type="button" data-toggle="modal" data-target="#newTaskModal" class="btn btn-primary" id="newTaskBtn">New</button>
         </li>
       </ul>
