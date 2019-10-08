fefcc8d5f6f5211df6a69e1b9b779b85328e9f2d
Pegasus <martingda@protonmail.com>
2019-10-08 10:17:41 -0300

    Reduced card width

--- webui/frontend/src/views/Login.vue
+++ webui/frontend/src/views/Login.vue
@@ -1,40 +1,47 @@
 <template>
+<div class="container">
   <div class="m-4 bg-dark card">
     <div class="card-header h4 font-weight-bold text-light">Sign in</div>
-    <div class="card-body container">
-      <div class="form-group text-left">
-        <label for="username" class="text-muted">Username</label>
-        <input
-          type="text"
-          class="form-control"
-          id="username"
-          aria-describedby="usernamelHelp"
-          placeholder="Enter your username"
-          v-model="username"
-        />
+    <div class="card-body">
+      <div class="p-4">
+        <div class="form-group text-left">
+          <label for="username" class="text-muted">Username</label>
+          <input
+            type="text"
+            class="form-control"
+            id="username"
+            aria-describedby="usernamelHelp"
+            placeholder="Enter your username"
+            v-model="username"
+          />
+        </div>
+        <div class="form-group text-left">
+          <label for="password" class="text-muted">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+            v-model="password"
+          />
+        </div>
+        <div class="form-group form-check">
+          <input type="checkbox" class="form-check-input" id="keepLoggedCheckbox" />
+          <label class="form-check-label text-light" for="keepLoggedCheckbox">Keep me logged</label>
+        </div>
+        
       </div>
-      <div class="form-group text-left">
-        <label for="password" class="text-muted">Password</label>
-        <input
-          type="password"
-          class="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-          v-model="password"
-        />
-      </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="keepLoggedCheckbox" />
-        <label class="form-check-label text-light" for="keepLoggedCheckbox">Keep me logged</label>
-      </div>
+    </div>
+    <div class="card-footer">
       <button 
         class="btn btn-primary btn-lg"
         @click="login"
         >
         Login
-        </button>
+      </button>
     </div>
   </div>
+</div>
 </template>
 
 <script>
