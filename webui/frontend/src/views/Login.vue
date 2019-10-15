f3e0a91b1965145743b54ddc35ea3add3cf334f4
Pegasus <martingda@protonmail.com>
2019-10-15 11:18:53 -0300

    Improved Login view design

--- webui/frontend/src/views/Login.vue
+++ webui/frontend/src/views/Login.vue
@@ -1,44 +1,44 @@
 <template>
-<div class="container">
-  <div class="m-4 bg-dark card">
-    <div class="card-header h4 font-weight-bold text-light">Sign in</div>
-    <div class="card-body">
-      <div class="p-2 p-md-4">
-        <div class="form-group text-left">
-          <label for="username" class="text-muted">Username</label>
-          <input
-            type="text"
-            class="form-control"
-            id="username"
-            aria-describedby="usernamelHelp"
-            placeholder="Enter your username"
-            v-model="username"
-          />
+<div class="container my-4">
+  <div class="justify-content-center row">
+    <div class="col-sm-9 col-md-7 col-lg-5 col-xl-4">
+      <div class="bg-dark card">
+        <div class="card-header h5 font-weight-bold text-light">Sign in</div>
+        <div class="card-body">
+          <form class>
+            <div class="form-group text-left">
+              <label for="username" class="text-light">Username</label>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                aria-describedby="usernamelHelp"
+                placeholder="Enter your username"
+                v-model="username"
+              />
+            </div>
+            <div class="form-group text-left">
+              <label for="passwordInput" class="text-light">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="passwordInput"
+                placeholder="Password"
+                v-model="password"
+              />
+            </div>
+            
+          </form>
         </div>
-        <div class="form-group text-left">
-          <label for="password" class="text-muted">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-            v-model="password"
-          />
+        <div class="card-footer">
+          <button 
+            class="btn btn-primary btn-lg btn-block"
+            @click="login"
+            >
+            Login
+          </button>
         </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="keepLoggedCheckbox" />
-          <label class="form-check-label text-light" for="keepLoggedCheckbox">Keep me logged</label>
-        </div>
-        
       </div>
-    </div>
-    <div class="card-footer">
-      <button 
-        class="btn btn-primary btn-lg"
-        @click="login"
-        >
-        Login
-      </button>
     </div>
   </div>
 </div>
