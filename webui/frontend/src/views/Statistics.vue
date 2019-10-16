ed846e6cb65956f726194a1c2c53b7eaed9f7a2d
Pegasus <martingda@protonmail.com>
2019-10-16 12:05:20 -0300

    Implementation of websocket + vuex

--- webui/frontend/src/views/Statistics.vue
+++ webui/frontend/src/views/Statistics.vue
@@ -13,6 +13,38 @@ export default {
   components: {
     appBoxes: Boxes,
     appPanels: Panels
+  },
+  activated () {
+    const socket = new WebSocket(`ws://${location.host}/ws`)
+
+    socket.onopen = () => {
+      console.info('Websocket connection established')
+    }
+
+    socket.onerror = (error) => {
+      console.error(error)
+    }
+
+    socket.onmessage = (event) => {
+      // Handle JSON data received from the server
+      const data = JSON.parse(event.data)
+
+      // Vuex usage
+      this.$store.dispatch('statistics/setActiveTasksCounter', data.activeTasks)
+      this.$store.dispatch('statistics/setOnExecutionTasksCounter', data.onExecutionTasks)
+      this.$store.dispatch('statistics/setInactiveTasksCounter', data.inactiveTasks)
+      this.$store.dispatch('statistics/setCompletedTasksCounter', data.completedTasks)
+
+      this.$store.dispatch('statistics/setAverageExecutionTime', data.averageExecutionTime)
+      this.$store.dispatch('statistics/setRunningTime', data.operatingTime)
+      this.$store.dispatch('statistics/setBackupLoopState', data.backupLoopState)
+
+      this.$store.dispatch('statistics/setRaspberryStatistics', data.raspberryStats)
+    }
+
+    socket.onclose = (event) => {
+     console.info("Websocket connection closed")
+    }
   }
 }
 </script>
