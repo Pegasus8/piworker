9f329f1d804c05944f08b59a13a3988029b2f49c
Pegasus <martingda@protonmail.com>
2019-10-22 10:27:19 -0300

    Changed Mutex to RWMutex

--- processment/configs/configModel.go
+++ processment/configs/configModel.go
@@ -7,7 +7,7 @@ import (
 
 // Configs is the struct used to store all PiWorker configurations.
 type Configs struct {
-	sync.Mutex
+	sync.RWMutex
 	Behavior Behavior `json:"behavior"`
 	Security Security `json:"security"`
 	Backups Backups `json:"backups"`
