b45ef1ae94d422a5cbd44decf061ea02aea48a61
Pegasus <martingda@protonmail.com>
2019-10-22 09:51:54 -0300

    Sync improvements on CurrentConfigs global var

--- processment/configs/users.go
+++ processment/configs/users.go
@@ -18,7 +18,10 @@ func NewUser(username, password string, admin bool) error {
 		hashedPassword,
 		admin,
 	}
+
+	CurrentConfigs.Lock()
 	CurrentConfigs.Users = append(CurrentConfigs.Users, newUser)
+	CurrentConfigs.Unlock()
 	err = WriteToFile()
 	if err != nil {
 		return err
@@ -31,11 +34,13 @@ func NewUser(username, password string, admin bool) error {
 func DeleteUser(username string) error {
 	// TODO Verify if the user that executes this function (from the API)
 	// is an admin user.
+	CurrentConfigs.Lock()
 	for index, user := range CurrentConfigs.Users {
 		if user.Username == username {
 			CurrentConfigs.Users = append(
 				CurrentConfigs.Users[:index], CurrentConfigs.Users[index+1:]...
 			)
+			CurrentConfigs.Unlock()
 			err := WriteToFile()
 			if err != nil {
 				return err
@@ -44,12 +49,15 @@ func DeleteUser(username string) error {
 			return nil
 		}
 	}
-
+	
+	CurrentConfigs.Unlock()
 	return ErrUserNotFound
 }
 
 // AuthUser is used to authenticate a user.
 func AuthUser(username, password string) (authenticated bool) {
+	CurrentConfigs.Lock()
+	defer CurrentConfigs.Unlock()
 	for _, user := range CurrentConfigs.Users {
 		if user.Username == username {
 			err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
@@ -64,6 +72,8 @@ func AuthUser(username, password string) (authenticated bool) {
 
 // ChangeUserPassword is a function used to change the password of a user.
 func ChangeUserPassword(username, newPassword string) error {
+	CurrentConfigs.Lock()
+
 	for _, user := range CurrentConfigs.Users {
 		if user.Username == username {
 			hashedPwd, err := hashAndSalt([]byte(newPassword))
@@ -71,6 +81,7 @@ func ChangeUserPassword(username, newPassword string) error {
 				return err
 			}
 			user.PasswordHash = hashedPwd
+			CurrentConfigs.Unlock()
 			err = WriteToFile()
 			if err != nil {
 				return err
@@ -79,10 +90,14 @@ func ChangeUserPassword(username, newPassword string) error {
 		}
 	}
 
+	CurrentConfigs.Unlock()
 	return ErrUserNotFound
 }
 
 func usernameExists(username string) bool {
+	CurrentConfigs.Lock()
+	defer CurrentConfigs.Unlock()
+
 	for _, user := range CurrentConfigs.Users {
 		if user.Username == username {
 			return true
