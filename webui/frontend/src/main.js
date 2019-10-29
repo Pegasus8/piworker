177dbc1494db5fb7a2e434ed36b8120752c1145c
Pegasus <martingda@protonmail.com>
2019-10-29 12:35:10 -0300

    Dynamic usage of http/https

--- webui/frontend/src/main.js
+++ webui/frontend/src/main.js
@@ -10,8 +10,30 @@ import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 Vue.use(UUID)
 
-// FIXME Check if https enabled on server
-axios.defaults.baseURL = `http://${location.host}`
+// By default use HTTP
+let protocol = 'http'
+
+// Change port
+let httpsCheckHost = location.host.replace(/:[\d]+/, ':8826')
+axios.get(`http://${httpsCheckHost}/https-check`)
+  .then((response) => {
+    if (response.data.enabled) {
+      // HTTPS support confirmed
+      protocol = 'https'
+      // Overwrittes base url, wich by default uses HTTP
+      // (remember that this is executed asynchronous)
+      axios.defaults.baseURL = `${protocol}://${location.host}`
+    } else {
+      console.info("PiWorker can't establish a HTTPS connection, using HTTP instead.")
+    }
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
+// By default use HTTP, if HTTPS are suported will be overwritten again
+// by the async request to the https-check URL
+axios.defaults.baseURL = `${protocol}://${location.host}`
 
 new Vue({
   router,
