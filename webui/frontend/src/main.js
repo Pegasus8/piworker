fc63a71aaac1b4cdda3767d12f62bfac7d63c673
Pegasus <martingda@protonmail.com>
2019-10-30 10:30:46 -0300

    Usage of location.protocol for base url

--- webui/frontend/src/main.js
+++ webui/frontend/src/main.js
@@ -10,30 +10,8 @@ import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 Vue.use(UUID)
 
-// By default use HTTP
-let protocol = 'http'
-
-// Change port
-let httpsCheckHost = location.host.replace(/:[\d]+/, ':8826')
-axios.get(`http://${httpsCheckHost}/https-check`)
-  .then((response) => {
-    if (response.data.enabled) {
-      // HTTPS support confirmed
-      protocol = 'https'
-      // Overwrittes base url, wich by default uses HTTP
-      // (remember that this is executed asynchronous)
-      axios.defaults.baseURL = `${protocol}://${location.host}`
-    } else {
-      console.info("PiWorker can't establish a HTTPS connection, using HTTP instead.")
-    }
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-
-// By default use HTTP, if HTTPS are suported will be overwritten again
-// by the async request to the https-check URL
-axios.defaults.baseURL = `${protocol}://${location.host}`
+// Use the protocol used to access the WebUI (HTTP/S)
+axios.defaults.baseURL = `${location.protocol}//${location.host}`
 
 new Vue({
   router,
