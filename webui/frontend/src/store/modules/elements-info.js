40a74c4b481daa81e5de1828ecd539b9cfb70c12
Pegasus <martingda@protonmail.com>
2019-11-11 12:07:49 -0300

    Changed indentation to 2 spaces (according to eslint)

--- webui/frontend/src/store/modules/elements-info.js
+++ webui/frontend/src/store/modules/elements-info.js
@@ -1,61 +1,65 @@
 import axios from 'axios'
 
 const state = {
-    triggers: [],
-    actions: []
+  triggers: [],
+  actions: []
 }
 
 const mutations = {
-    setTriggers: (state, newTriggers) => {
-        state.triggers = newTriggers
-    },
-    setActions: (state, newActions) => {
-        state.actions = newActions
-    }
+  setTriggers: (state, newTriggers) => {
+    state.triggers = newTriggers
+  },
+  setActions: (state, newActions) => {
+    state.actions = newActions
+  }
 }
 
 const actions = {
-    updateTriggersInfo: ({ commit }) => {
-        console.info('Updating triggers info...')
-        axios.get('/api/webui/triggers-structs')
-            .then((response) => {
-                console.info('Good response from triggers-structs API, parsing triggers...')
-                const triggersStructs = response.data
-                commit('setTriggers', triggersStructs)
-                console.info('Triggers info updated successfully!')
-            })
-            .catch((err) => {
-                console.error('Error on triggers-structs API',err)
-            }) 
-    },
-    updateActionsInfo: ({ commit }) => {
-        console.info('Updating actions info...')
-        axios.get('/api/webui/actions-structs')
-            .then((response) => {
-                console.info('Good response from actions-structs API, parsing actions...')
-                const actionsStructs = response.data
-                commit('setActions', actionsStructs)
-                console.info('Actions info updated successfully!')
-            })
-            .catch((err) => {
-                console.error('Error on actions-structs API',err)
-            })
-    }
+  updateTriggersInfo: ({
+    commit
+  }) => {
+    console.info('Updating triggers info...')
+    axios.get('/api/webui/triggers-structs')
+      .then((response) => {
+        console.info('Good response from triggers-structs API, parsing triggers...')
+        const triggersStructs = response.data
+        commit('setTriggers', triggersStructs)
+        console.info('Triggers info updated successfully!')
+      })
+      .catch((err) => {
+        console.error('Error on triggers-structs API', err)
+      })
+  },
+  updateActionsInfo: ({
+    commit
+  }) => {
+    console.info('Updating actions info...')
+    axios.get('/api/webui/actions-structs')
+      .then((response) => {
+        console.info('Good response from actions-structs API, parsing actions...')
+        const actionsStructs = response.data
+        commit('setActions', actionsStructs)
+        console.info('Actions info updated successfully!')
+      })
+      .catch((err) => {
+        console.error('Error on actions-structs API', err)
+      })
+  }
 }
 
 const getters = {
-    triggers: (state) => {
-        return state.triggers
-    },
-    actions: (state) => {
-        return state.actions
-    }
+  triggers: (state) => {
+    return state.triggers
+  },
+  actions: (state) => {
+    return state.actions
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
 }
--- webui/frontend/src/store/modules/user-tasks.js
+++ webui/frontend/src/store/modules/user-tasks.js
@@ -1,40 +1,42 @@
 import axios from 'axios'
 
 const state = {
-    tasks: []
+  tasks: []
 }
 
 const mutations = {
-    updateTasks: (state, updatedTasks) => {
-        state.tasks = updatedTasks
-    }
+  updateTasks: (state, updatedTasks) => {
+    state.tasks = updatedTasks
+  }
 }
 
 const actions = {
-    getUserTasks: ({ commit }) => {
-        console.info('Sending request to get-all tasks API...');
-        axios.get('/api/tasks/get-all')
-            .then((response) => {
-                console.info('Response successful, parsing tasks...');
-                commit('updateTasks', response.data)
-                console.info('Tasks parsed!')
-            })
-            .catch((err) => {
-                console.error('Error on get-all tasks API:', err)
-            })
-    }
+  getUserTasks: ({
+    commit
+  }) => {
+    console.info('Sending request to get-all tasks API...');
+    axios.get('/api/tasks/get-all')
+      .then((response) => {
+        console.info('Response successful, parsing tasks...');
+        commit('updateTasks', response.data)
+        console.info('Tasks parsed!')
+      })
+      .catch((err) => {
+        console.error('Error on get-all tasks API:', err)
+      })
+  }
 }
 
 const getters = {
-    tasks: (state) => {
-        return state.tasks
-    }
+  tasks: (state) => {
+    return state.tasks
+  }
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions,
-    getters
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
 }
