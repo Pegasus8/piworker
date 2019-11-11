58fca071162f50e8853b49f739e35eb449c26a16
Pegasus <martingda@protonmail.com>
2019-11-11 12:05:04 -0300

    Bug fixed and code formatting

--- webui/frontend/src/components/new-task/FormGroupContainer.vue
+++ webui/frontend/src/components/new-task/FormGroupContainer.vue
@@ -2,43 +2,46 @@
   <div class="form-group card p-0 bg-dark">
     <div class="card-body">
       <label :for="topElementID" class="card-title text-light">{{ containerTitle }}</label>
-      <slot name="top"/>
-      <small :id='topElementID + "-description"' class="form-text text-muted text-left">{{ containerDescription }}</small>
+      <slot name="top" />
+      <small
+        :id="topElementID + '-description'"
+        class="form-text text-muted text-left"
+      >{{ containerDescription }}</small>
     </div>
     <div v-if="withFooter" class="card-footer">
-      <slot name="bottom"/>
+      <slot name="bottom" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-      containerTitle: {
-          type: String,
-          required: true,
-          default: ''
-      },
-      containerDescription: {
-          type: String,
-          required: true,
-          default: ''
-      },
-      topElementID: {
-        type: String,
-        required: true,
-        default: ''
-      },
-      withFooter: {
-        type: Boolean,
-        required: false,
-        default: false
-      }
+  props: {
+    containerTitle: {
+      type: String,
+      required: true,
+      default: ""
     },
-    data() {
-      return {
-        hasBottomSlot: false
-      }
+    containerDescription: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    topElementID: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    withFooter: {
+      type: Boolean,
+      required: false,
+      default: false
     }
-}
+  },
+  data() {
+    return {
+      hasBottomSlot: false
+    };
+  }
+};
 </script>
