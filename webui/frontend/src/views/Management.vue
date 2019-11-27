13691159e42aa63d91f15bdfe5e5300e51123ce9
Pegasus <martingda@protonmail.com>
2019-11-27 11:29:24 -0300

    Added danger alert on request error

--- webui/frontend/src/views/Management.vue
+++ webui/frontend/src/views/Management.vue
@@ -12,9 +12,14 @@
       />
       <!-- TODO Logs integration -->
     </b-container>
-    <b-alert v-else variant="warning" class="m-4">
+    <b-alert v-else variant="warning" class="m-4" fade>
       Oops... It seems that you have not created any task yet.
       Let's click on the "New" button to create a new one!
+    </b-alert>
+    <b-alert :show="err != ''" variant="danger" @dismissed="err = ''" class="floating-alert"  dismissible fade>
+      <h5>Error when getting info</h5>
+      <hr>
+      <p>{{ err }}</p>
     </b-alert>
   </b-container>
 </template>
@@ -27,6 +32,7 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
+      err: ""
     }
   },
   computed: {
@@ -40,12 +46,18 @@ export default {
   beforeCreate () {
     if (!this.$store.getters['elementsInfo/triggers'].length > 0) {
       this.$store.dispatch('elementsInfo/updateTriggersInfo')
+        .catch((error) => {
+          this.err = 'Error on trigger-structs API: ' + error
+        })
     }
     if (!this.$store.getters['elementsInfo/actions'].length > 0) {
       this.$store.dispatch('elementsInfo/updateActionsInfo')
+        .catch((error) => {
+          this.err = 'Error on actions-structs API: ' + error
+        })
     }
     this.$store.dispatch('userTasks/getUserTasks')
-  },
+  }
 }
 </script>
 
