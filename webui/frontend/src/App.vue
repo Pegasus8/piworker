be536ea42e7c0c18034beb64464e2ae019340f95
Pegasus <martingda@protonmail.com>
2020-01-22 19:55:57 -0300

    Partially migrated to vuetify components

--- webui/frontend/src/App.vue
+++ webui/frontend/src/App.vue
@@ -1,34 +1,43 @@
 <template>
-  <div id="app">
-    <app-navbar/>
-    <transition
-      name="fade"
-      mode="out-in"
-    >
-      <router-view style="animation-duration: 0.3s"/>
-    </transition>
-  </div>
+  <v-app>
+    <app-navigation-drawer
+      :expanded="expandNavDrawer"
+    />
+    <app-navbar
+      @expandDrawer='expandNavDrawer = !expandNavDrawer'
+    />
+    <v-content>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view style="animation-duration: 0.3s"/>
+      </transition>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Navbar from './components/navbar/Navbar.vue'
+import Navbar from './components/shared/navbar/Navbar.vue'
+import NavigationDrawer from './components/shared/navigation-drawer/NavigationDrawer.vue'
 
 export default {
+  data () {
+    return {
+      expandNavDrawer: false
+    }
+  },
+  methods: {
+  },
   components: {
-    appNavbar: Navbar
+    appNavbar: Navbar,
+    appNavigationDrawer: NavigationDrawer
   }
 }
 </script>
 
 <style lang="scss">
 @import url("assets/css/icomoon.css");
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
 
 .floating-alert {
   position: fixed;
