811e19c101124439b51c60bf22435f078fc8f7df
Pegasus <martingda@protonmail.com>
2020-01-22 19:55:02 -0300

    Initialized navigation drawer

---
+++ webui/frontend/src/components/shared/navigation-drawer/NavigationDrawer.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-navigation-drawer hide-overlay :value='expanded' app>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: {
+    expanded: {
+      required: true,
+      type: Boolean
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
