8466c9f4ddf9425f1dfb17b24042c814c4471b46
Pegasus <martingda@protonmail.com>
2020-01-22 21:54:59 -0300

    Added items

--- webui/frontend/src/components/shared/navigation-drawer/NavigationDrawer.vue
+++ webui/frontend/src/components/shared/navigation-drawer/NavigationDrawer.vue
@@ -1,17 +1,56 @@
 <template>
-  <v-navigation-drawer hide-overlay :value='expanded' app>
+  <v-navigation-drawer v-model='expandNavDrawer' fixed app>
+    <v-list-item-group v-model="activeView">
+      <v-list-item :to="{name: 'statistics'}" @click="expandNavDrawer = false">
+        <v-list-item-title>
+          Statistics
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item :to="{name: 'management'}" @click="expandNavDrawer = false">
+        <v-list-item-title>
+          Management
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item :to="{name: 'settings'}" @click="expandNavDrawer = false">
+        <v-list-item-title>
+          Settings
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item :to="{name: 'new-task'}" @click="expandNavDrawer = false">
+        <v-list-item-title>
+          New Task
+        </v-list-item-title>
+      </v-list-item>
+    </v-list-item-group>
   </v-navigation-drawer>
 </template>
 <script>
 export default {
   data () {
     return {
+      activeView: null
     }
   },
   props: {
     expanded: {
       required: true,
       type: Boolean
+    }
+  },
+  computed: {
+    expandNavDrawer: {
+      get () {
+        return this.expanded
+      },
+      set (newState) {
+        this.$emit('stateChange', newState)
+      }
+    }
+  },
+  watch: {
+    activeView: function (newValue) {
+      // When the view is closed close the navigation drawer
+      // this.expandNavDrawer = false
     }
   }
 }
