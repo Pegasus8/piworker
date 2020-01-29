2be7c3a852e01299e0f6759528c05e1af311385b
Pegasus <martingda@protonmail.com>
2020-01-29 20:39:52 -0300

    Added component AdaptativeArgSelector for NewTask view

---
+++ webui/frontend/src/components/new-task/AdaptativeArgSelector.vue
@@ -0,0 +1,37 @@
+<template>
+<v-text-field
+  v-model.lazy='argContent'
+  :rules='[
+    v => !!v || "Empty args are not allowed"
+  ]'
+  required
+/>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    argType: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    argContent: {
+      get () {
+        return this.content
+      },
+      set (newValue) {
+        this.$emit('changed', newValue)
+      }
+    }
+  },
+  mounted () {
+    console.log(this.content)
+  }
+}
+</script>
