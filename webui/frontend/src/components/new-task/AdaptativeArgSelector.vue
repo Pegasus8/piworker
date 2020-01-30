f579e00df988f90d9772a9fd634d3429b80005cc
Pegasus <martingda@protonmail.com>
2020-01-30 13:32:34 -0300

    Improved arg selector
    Added compatibility with pwtypes (not totally, but at least partially).

--- webui/frontend/src/components/new-task/AdaptativeArgSelector.vue
+++ webui/frontend/src/components/new-task/AdaptativeArgSelector.vue
@@ -1,11 +1,56 @@
 <template>
-<v-text-field
-  v-model.lazy='argContent'
-  :rules='[
-    v => !!v || "Empty args are not allowed"
-  ]'
-  required
-/>
+<div>
+  <v-text-field
+    v-if="
+      argType == 'url' ||
+      argType == 'path' ||
+      argType == 'number' ||
+      argType == 'any'
+    "
+    :type='argType == "number"? "number":"text"'
+    v-model.lazy='argContent'
+    :rules='[
+      rules.emptyNotAllowed
+    ]'
+    required
+  />
+  <v-textarea
+    v-if="
+      argType == 'text' ||
+      argType == 'json'
+    "
+    v-model.lazy='argContent'
+    :rules='[
+      rules.emptyNotAllowed
+    ]'
+    clear-icon='cancel'
+    filled
+    auto-grow
+    clearable
+  />
+  <v-date-picker
+    v-if="argType == 'date'"
+    v-model.lazy="argContent"
+    :min='getDate()'
+    :rules='[
+      rules.emptyNotAllowed
+    ]'
+  />
+  <v-time-picker
+    v-if='argType == "time"'
+    v-model.lazy="argContent"
+    :rules='[
+      rules.emptyNotAllowed
+    ]'
+  />
+  <v-checkbox
+    v-if='argType == "boolean"'
+    v-model="argContent"
+    :rules='[
+      rules.emptyNotAllowed
+    ]'
+  />
+</div>
 </template>
 
 <script>
@@ -20,6 +65,16 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      rules: {
+        emptyNotAllowed: v => !!v || 'Empty args are not allowed'
+      },
+
+      datePickerModel: null,
+      timePickerModel: null
+    }
+  },
   computed: {
     argContent: {
       get () {
@@ -28,6 +83,16 @@ export default {
       set (newValue) {
         this.$emit('changed', newValue)
       }
+    }
+  },
+  methods: {
+    getDate () {
+      const today = new Date()
+      const dd = String(today.getDate()).padStart(2, '0')
+      const mm = String(today.getMonth() + 1).padStart(2, '0') // January is 0!
+      const yyyy = today.getFullYear()
+
+      return yyyy + '-' + mm + '-' + dd
     }
   }
 }
