b5480a78b2f8a7c39de858ffb6fa690d50d19c9e
Pegasus <martingda@protonmail.com>
2020-02-14 17:51:54 -0300

    Usage of task ID on task loop execution

--- processment/engine/taskExecution.go
+++ processment/engine/taskExecution.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/Pegasus8/piworker/processment/data"
@@ -17,59 +16,59 @@ import (
 	"github.com/Pegasus8/piworker/processment/uservariables"
 )
 
-func runTaskLoop(taskname string, taskChannel chan data.UserTask) {
-	log.Printf("[%s] Loop started\n", taskname)
+func runTaskLoop(taskID string, taskChannel chan data.UserTask) {
+	log.Printf("[%s] Loop started\n", taskID)
 	for {
 		// Receive the renewed data for the task in question, if there is not data
 		// just keep waiting for it.
 		taskReceived := <-taskChannel
 
-		triggered, err := runTrigger(taskReceived.TaskInfo.Trigger, taskReceived.TaskInfo.Name)
+		triggered, err := runTrigger(taskReceived.TaskInfo.Trigger, taskReceived.TaskInfo.ID)
 		if err != nil {
 			log.Fatalf("[%s] Error while trying to run the trigger of the task, stopping the task execution...\n",
-				taskReceived.TaskInfo.Name)
+				taskReceived.TaskInfo.ID)
 		}
 		if triggered {
-			if wasRecentlyExecuted(taskReceived.TaskInfo.Name) {
+			if wasRecentlyExecuted(taskReceived.TaskInfo.ID) {
 				log.Printf("[%s] The task was recently executed, the trigger "+
-					"stills active. Skipping it...\n", taskReceived.TaskInfo.Name)
+					"stills active. Skipping it...\n", taskReceived.TaskInfo.ID)
 				goto skipTaskExecution
 			}
 
 			log.Printf("[%s] Trigger with the ID '%s' activated, running actions...\n",
-				taskReceived.TaskInfo.Name, taskReceived.TaskInfo.Trigger.ID)
+				taskReceived.TaskInfo.ID, taskReceived.TaskInfo.Trigger.ID)
 			runActions(&taskReceived)
 
-			err = setAsRecentlyExecuted(taskReceived.TaskInfo.Name)
+			err = setAsRecentlyExecuted(taskReceived.TaskInfo.ID)
 			if err != nil {
-				log.Printf("[%s] %s\n", taskReceived.TaskInfo.Name, err.Error())
+				log.Printf("[%s] %s\n", taskReceived.TaskInfo.ID, err.Error())
 			}
 
 			skipTaskExecution:
 				// Skip the execution of the task but not skip the entire iteration
 				// in case of have to do something else with the task.
 		} else {
-			if wasRecentlyExecuted(taskReceived.TaskInfo.Name) {
-				err = setAsReadyToExecuteAgain(taskReceived.TaskInfo.Name)
+			if wasRecentlyExecuted(taskReceived.TaskInfo.ID) {
+				err = setAsReadyToExecuteAgain(taskReceived.TaskInfo.ID)
 				if err != nil {
-					log.Printf("[%s] %s\n", taskReceived.TaskInfo.Name, err.Error())
+					log.Printf("[%s] %s\n", taskReceived.TaskInfo.ID, err.Error())
 				}
 			}
 		}
 	}
 }
 
-func runTrigger(trigger data.UserTrigger, parentTaskName string) (bool, error) {
+func runTrigger(trigger data.UserTrigger, parentTaskID string) (bool, error) {
 	for _, pwTrigger := range triggersList.TRIGGERS {
 		if trigger.ID == pwTrigger.ID {
 			for _, arg := range trigger.Args {
 				// Check if the arg contains a user global variable
-				err := searchAndReplaceVariable(&arg, parentTaskName)
+				err := searchAndReplaceVariable(&arg, parentTaskID)
 				if err != nil {
 					return false, err
 				}
 			}
-			result, err := pwTrigger.Run(&trigger.Args, parentTaskName)
+			result, err := pwTrigger.Run(&trigger.Args, parentTaskID)
 			if err != nil {
 				return false, err
 			}
@@ -84,18 +83,18 @@ func runTrigger(trigger data.UserTrigger, parentTaskName string) (bool, error) {
 }
 
 func runActions(task *data.UserTask) {
-	log.Printf("[%s] Running actions...\n", task.TaskInfo.Name)
+	log.Printf("[%s] Running actions...\n", task.TaskInfo.ID)
 	startTime := time.Now()
 
 	userActions := &task.TaskInfo.Actions
 	previousState := task.TaskInfo.State
 
-	log.Printf("[%s] Changing task state to '%s'\n", task.TaskInfo.Name, data.StateTaskOnExecution)
+	log.Printf("[%s] Changing task state to '%s'\n", task.TaskInfo.ID, data.StateTaskOnExecution)
 	// Set task state to on-execution
-	err := data.UpdateTaskState(task.TaskInfo.Name, data.StateTaskOnExecution)
+	err := data.UpdateTaskState(task.TaskInfo.ID, data.StateTaskOnExecution)
 	if err != nil {
 		log.Fatalf("[%s] Error when trying to update the task state to '%s'\n",
-			task.TaskInfo.Name, data.StateTaskOnExecution)
+			task.TaskInfo.ID, data.StateTaskOnExecution)
 	}
 
 	var chainedResult *actionsModel.ChainedResult
@@ -108,37 +107,37 @@ func runActions(task *data.UserTask) {
 				// Run the action
 				for _, action := range actionsList.ACTIONS {
 					if userAction.ID == action.ID {
-						log.Printf("[%s] Running action n%d. Chained: %t | Previous chained result: %+v\n", task.TaskInfo.Name, orderN, userAction.Chained, chainedResult)
+						log.Printf("[%s] Running action n%d. Chained: %t | Previous chained result: %+v\n", task.TaskInfo.ID, orderN, userAction.Chained, chainedResult)
 
 						for _, arg := range userAction.Args {
-							err := searchAndReplaceVariable(&arg, task.TaskInfo.Name)
+							err := searchAndReplaceVariable(&arg, task.TaskInfo.ID)
 							if err != nil {
-								log.Printf("[%s] %s\n", task.TaskInfo.Name, err.Error())
+								log.Printf("[%s] %s\n", task.TaskInfo.ID, err.Error())
 								return
 							}
 						}
 
 						ua, err := replaceArgByCR(chainedResult, &userAction)
 						if err != nil {
-							log.Printf("[%s] %s\n", task.TaskInfo.Name, err.Error())
+							log.Printf("[%s] %s\n", task.TaskInfo.ID, err.Error())
 							return
 						}
 						userAction = *ua
 
-						result, chr, err := action.Run(chainedResult, &userAction, task.TaskInfo.Name)
+						result, chr, err := action.Run(chainedResult, &userAction, task.TaskInfo.ID)
 						// Set the returned chr (chained result) to our main instance of the ChainedResult struct (`chainedResult`).
 						// This will be given to the next action (if exists).
 						chainedResult = chr
 						if err != nil {
-							log.Printf("[%s] %s\n", task.TaskInfo.Name, err.Error())
+							log.Printf("[%s] %s\n", task.TaskInfo.ID, err.Error())
 							return
 						}
 						if result {
 							log.Printf("[%s] Action in order %d finished correctly\n",
-								task.TaskInfo.Name, userAction.Order)
+								task.TaskInfo.ID, userAction.Order)
 						} else {
 							log.Printf("[%s] Action in order %d wasn't executed correctly. Aborting task for prevention of future errors...\n",
-								task.TaskInfo.Name, userAction.Order)
+								task.TaskInfo.ID, userAction.Order)
 							return
 						}
 
@@ -159,20 +158,20 @@ func runActions(task *data.UserTask) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	updatedTask, _, err := updatedData.GetTaskByName(task.TaskInfo.Name)
+	updatedTask, _, err := updatedData.GetTaskByID(task.TaskInfo.ID)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	lastState := updatedTask.TaskInfo.State
 	// If the state has no changes, return to the original state
 	if lastState == data.StateTaskOnExecution {
-		err = data.UpdateTaskState(task.TaskInfo.Name, previousState)
+		err = data.UpdateTaskState(task.TaskInfo.ID, previousState)
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
 	executionTime := time.Since(startTime).String()
-	log.Printf("[%s] Actions executed in %s\n", task.TaskInfo.Name, executionTime)
+	log.Printf("[%s] Actions executed in %s\n", task.TaskInfo.ID, executionTime)
 }
 
 func checkForAnUpdate(updateChannel chan bool) {
@@ -200,14 +199,13 @@ func checkForAnUpdate(updateChannel chan bool) {
 	}
 }
 
-func setAsRecentlyExecuted(taskName string) error {
-	taskName = strings.ReplaceAll(taskName, " ", "_")
+func setAsRecentlyExecuted(ID string) error {
 	dir, err := ioutil.TempDir(TempDir, "")
 	if err != nil {
 		return err
 	}
 
-	file, err := ioutil.TempFile(filepath.Join(dir, taskName), "")
+	file, err := ioutil.TempFile(filepath.Join(dir, ID), "")
 	if err != nil {
 		return err
 	}
@@ -216,25 +214,23 @@ func setAsRecentlyExecuted(taskName string) error {
 	return nil
 }
 
-func wasRecentlyExecuted(taskName string) bool {
-	taskName = strings.ReplaceAll(taskName, " ", "_")
-	_, err := os.Stat(filepath.Join(TempDir, taskName))
+func wasRecentlyExecuted(ID string) bool {
+	_, err := os.Stat(filepath.Join(TempDir, ID))
 	if err != nil {
 		if os.IsNotExist(err) {
 			return false
 		} else if os.IsExist(err) {
 			return true
 		}
-		log.Printf("[%s] %s\n", taskName, err.Error())
+		log.Printf("[%s] %s\n", ID, err.Error())
 		return false
 	}
 
 	return true
 }
 
-func setAsReadyToExecuteAgain(taskName string) error {
-	taskName = strings.ReplaceAll(taskName, " ", "_")
-	path := filepath.Join(TempDir, taskName)
+func setAsReadyToExecuteAgain(ID string) error {
+	path := filepath.Join(TempDir, ID)
 	err := os.Remove(path)
 	if err != nil {
 		return err
@@ -243,7 +239,7 @@ func setAsReadyToExecuteAgain(taskName string) error {
 	return nil
 }
 
-func searchAndReplaceVariable(arg *data.UserArg, parentTaskName string) error {
+func searchAndReplaceVariable(arg *data.UserArg, parentTaskID string) error {
 	// Check if the arg contains a user global variable
 	if uservariables.ContainGlobalVariable(&arg.Content) {
 		// If yes, then get the name of the variable by using regex
@@ -251,7 +247,7 @@ func searchAndReplaceVariable(arg *data.UserArg, parentTaskName string) error {
 		// Get the variable from the name
 		globalVar, err := uservariables.GetGlobalVariable(varName)
 		if err != nil {
-			log.Printf("[%s] Error when trying to read the user global variable '%s': %s\n", parentTaskName, varName, err.Error())
+			log.Printf("[%s] Error when trying to read the user global variable '%s': %s\n", parentTaskID, varName, err.Error())
 			return err
 		}
 		globalVar.RLock()
@@ -264,9 +260,9 @@ func searchAndReplaceVariable(arg *data.UserArg, parentTaskName string) error {
 		// If yes, then get the name of the variable by using regex
 		varName := uservariables.GetLocalVariableName(arg.Content)
 		// Get the variable from the name
-		localVariable, err := uservariables.GetLocalVariable(varName, parentTaskName)
+		localVariable, err := uservariables.GetLocalVariable(varName, parentTaskID)
 		if err != nil {
-			log.Printf("[%s] Error when trying to read the user local variable '%s': %s\n", parentTaskName, varName, err.Error())
+			log.Printf("[%s] Error when trying to read the user local variable '%s': %s\n", parentTaskID, varName, err.Error())
 			return err
 		}
 		localVariable.RLock()
