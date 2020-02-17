b70ec1c82a3cf484972ad7aa6d82595f1135377d
Pegasus <martingda@protonmail.com>
2020-02-17 10:03:53 -0300

    Improved aspect ratio of boxes

--- webui/frontend/src/components/statistics/Boxes.vue
+++ webui/frontend/src/components/statistics/Boxes.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-container class="my-4" fluid>
-    <v-row class="justify-content-center mx-5 p-4">
+  <v-container class="my-4">
+    <v-row justify='center' class="mx-5 p-4">
       <v-col
-        cols="10"
-        sm="6"
-        md="5"
+        cols="7"
+        sm="4"
         lg="3"
+        xl="2"
         class="m-3 mx-md-4 mx-lg-5 py-4"
       >
         <div id="atcBox">
           <v-card elevation='6' class="p-4">
-            <h4 class="text-muted text-center">Active</h4>
-            <v-card-subtitle class="display-4 white--text text-center">
+            <v-card-title class="d-flex justify-center">
+              <h4 class="text-muted">Active</h4>
+            </v-card-title>
+            <v-card-subtitle class="display-4 text-center my-1">
               {{ activeTasksCounter }}
             </v-card-subtitle>
           </v-card>
@@ -19,16 +21,18 @@
       </v-col>
 
       <v-col
-        cols="10"
-        sm="6"
-        md="5"
+        cols="7"
+        sm="4"
         lg="3"
+        xl="2"
         class="m-3 mx-md-4 mx-lg-5 py-4"
       >
         <div id="oetcBox">
           <v-card elevation='6' class="p-4">
-            <h4 class="text-muted text-center">Running...</h4>
-            <v-card-subtitle class="display-4 white--text text-center">
+            <v-card-title class="d-flex justify-center">
+              <h4 class="text-muted">Running...</h4>
+            </v-card-title>
+            <v-card-subtitle class="display-4 text-center my-1">
               {{ onExecutionTasksCounter }}
             </v-card-subtitle>
           </v-card>
@@ -36,16 +40,18 @@
       </v-col>
 
       <v-col
-        cols="10"
-        sm="6"
-        md="5"
+        cols="7"
+        sm="4"
         lg="3"
+        xl="2"
         class="m-3 mx-md-4 mx-lg-5 py-4"
       >
         <div id="itcBox">
           <v-card elevation='6' class="p-4">
-            <h4 class="text-muted text-center">Inactive</h4>
-            <v-card-subtitle class="display-4 white--text text-center">
+            <v-card-title class="d-flex justify-center">
+              <h4 class="text-muted">Inactive</h4>
+            </v-card-title>
+            <v-card-subtitle class="display-4 text-center my-1">
               {{ inactiveTasksCounter }}
             </v-card-subtitle>
           </v-card>
