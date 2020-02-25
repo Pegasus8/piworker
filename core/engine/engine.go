022bd0d844c8dddbd0fd2d138d543d930dbf85fb
Pegasus <martingda@protonmail.com>
2020-02-25 14:48:55 -0300

    Usage of `signals.Shutdown` channel

--- core/engine/engine.go
+++ core/engine/engine.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Pegasus8/piworker/core/configs"
 	"github.com/Pegasus8/piworker/core/data"
+	"github.com/Pegasus8/piworker/core/signals"
 	"github.com/Pegasus8/piworker/core/stats"
 	"github.com/Pegasus8/piworker/webui/backend"
 	"github.com/rs/zerolog/log"
@@ -56,49 +57,53 @@ func StartEngine() {
 	configs.CurrentConfigs.RUnlock()
 
 	// Keep the data updated
-	for range time.Tick(tickDuration) {
-		select {
-		case <-needUpdateData:
-			{
-				log.Info().
-					Msg("Updating the data variable due to a change detected...")
-				// Renew the data variable.
-				userData, err = data.ReadData()
-				if err != nil {
-					log.Fatal().
-						Err(err).
-						Msg("Error when trying to update the data on the engine")
+	go func() {
+		for range time.Tick(tickDuration) {
+			select {
+			case <-needUpdateData:
+				{
+					log.Info().
+						Msg("Updating the data variable due to a change detected...")
+					// Renew the data variable.
+					userData, err = data.ReadData()
+					if err != nil {
+						log.Fatal().
+							Err(err).
+							Msg("Error when trying to update the data on the engine")
+					}
+
+					log.Info().Msg("Data variable updated successfully")
 				}
-
-				log.Info().Msg("Data variable updated successfully")
+			default:
+				// Keep using the current data.
 			}
-		default:
-			// Keep using the current data.
-		}
 
-		select {
-		case dataChannel <- *userData:
-			// Send the data to the stats loop.
-		default:
-			// If casually the loop is not awaiting for it, continue the loop for prevention
-			// of blocking and delay.
-		}
+			select {
+			case dataChannel <- *userData:
+				// Send the data to the stats loop.
+			default:
+				// If casually the loop is not awaiting for it, continue the loop for prevention
+				// of blocking and delay.
+			}
 
-		for _, task := range userData.Tasks {
-			if task.TaskInfo.State != data.StateTaskActive {
-				// Skip the task
-				continue
+			for _, task := range userData.Tasks {
+				if task.TaskInfo.State != data.StateTaskActive {
+					// Skip the task
+					continue
+				}
+				taskID := task.TaskInfo.ID
+				// Check if the task loop and channel have already been initialized
+				if _, alreadyExists := tasksGoroutines[taskID]; !alreadyExists {
+					// Initialize the channel
+					tasksGoroutines[taskID] = make(chan data.UserTask)
+					// Start the loop
+					go runTaskLoop(taskID, tasksGoroutines[taskID])
+				}
+				// Send the data to the task's channel
+				tasksGoroutines[taskID] <- task
 			}
-			taskID := task.TaskInfo.ID
-			// Check if the task loop and channel have already been initialized
-			if _, alreadyExists := tasksGoroutines[taskID]; !alreadyExists {
-				// Initialize the channel
-				tasksGoroutines[taskID] = make(chan data.UserTask)
-				// Start the loop
-				go runTaskLoop(taskID, tasksGoroutines[taskID])
-			}
-			// Send the data to the task's channel
-			tasksGoroutines[taskID] <- task
 		}
-	}
+	}()
+
+	<-signals.Shutdown
 }
