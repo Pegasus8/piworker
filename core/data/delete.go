508bfd46768b63b7918b8283048727df233405e9
Pegasus <martingda@protonmail.com>
2020-03-03 11:23:49 -0300

    Reimplemented `DeleteTask` fn with the use of db

--- core/data/delete.go
+++ core/data/delete.go
@@ -1,40 +1,18 @@
 package data
 
 import (
-	"encoding/json"
-
-	"github.com/Pegasus8/piworker/utilities/files"
 	"github.com/rs/zerolog/log"
 )
 
-// DeleteTask is a function used to delete a specific task from the JSON user database.
+// DeleteTask is a function used to delete a specific task from the table 'Tasks', on the SQLite3 database.
 func DeleteTask(ID string) error {
+	sqlStatement := `
+		DELETE FROM Tasks
+		WHERE ID = ?;
+	`
 	log.Info().Str("taskID", ID).Msg("Deleting task...")
 
-	data, err := ReadData()
-	if err != nil {
-		return err
-	}
-
-	_, index, err := data.GetTaskByID(ID)
-	if err != nil {
-		return err
-	}
-	log.Info().Str("taskID", ID).Msg("Task found, deleting...")
-
-	if len(data.Tasks) == 1 {
-		data.Tasks = []UserTask{}
-	} else {
-		data.Tasks = append(data.Tasks[:index], data.Tasks[index+1:]...)
-	}
-
-	byteData, err := json.MarshalIndent(data, "", "   ")
-	if err != nil {
-		return err
-	}
-
-	// Re-write data into file
-	_, err = files.WriteFile(DataPath, Filename, byteData)
+	_, err := DB.Exec(sqlStatement, ID)
 	if err != nil {
 		return err
 	}
