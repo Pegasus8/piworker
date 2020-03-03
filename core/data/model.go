9b59e0c3ee2f4ea25b64f132f075f995164f7dab
Pegasus <martingda@protonmail.com>
2020-03-03 11:18:34 -0300

    Simplified task related structs

--- core/data/model.go
+++ core/data/model.go
@@ -2,18 +2,8 @@ package data
 
 import "time"
 
-// UserData is the general struct for parsing data
-type UserData struct {
-	Tasks []UserTask `json:"user-data"`
-}
-
-// UserTask is the structure used for parsing all the tasks
+// UserTask is a struct for parse each task.
 type UserTask struct {
-	TaskInfo TaskInfo `json:"task"`
-}
-
-// TaskInfo is a struct for parsing every task
-type TaskInfo struct {
 	Name             string       `json:"name"`
 	State            TaskState    `json:"state"`
 	Trigger          UserTrigger  `json:"trigger"`
@@ -37,7 +27,7 @@ type UserAction struct {
 	Timestamp             string    `json:"timestamp"`
 	Chained               bool      `json:"chained"`
 	ArgumentToReplaceByCR string    `json:"argumentToReplaceByCR"`
-	Order                 int8       `json:"order"`
+	Order                 uint8     `json:"order"`
 }
 
 // UserArg is a struct for arg parsing
