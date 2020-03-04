6c583389d1fe34f5c7fb7e46f19f1e9001dcdcb1
Pegasus <martingda@protonmail.com>
2020-03-04 12:06:12 -0300

    Added variable `Stats` for tasks statistics

--- core/engine/variables.go
+++ core/engine/variables.go
@@ -9,3 +9,6 @@ var (
 	// TempDir is the directory used to store the temp files of the Dynamic Engine
 	TempDir = filepath.Join(os.TempDir(), "/PiWorker/")
 )
+
+// Stats is the variable that holds the different statistics of the tasks's execution.
+var Stats TasksStats
