3aa812ca40d4ad9a31b9feab9e8df216e3cbfd22
Pegasus <martingda@protonmail.com>
2020-03-10 11:38:16 -0300

    Replaced `GetStatistics` fn by `UpdateRPiStats`
    Due to the new way of stats recolection, there is no necessity of constantly read the entire db of tasks (events are totally helping on this). The unique operation to do expressly is the recolection of stats related with the host. Task executed by the new function `UpdateRPiStats`.

--- core/stats/read.go
+++ core/stats/read.go
@@ -1,164 +1,59 @@
 package stats
 
 import (
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"regexp"
-	"strconv"
-
-	"github.com/Pegasus8/piworker/core/data"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
 )
 
 // GetStatistics is a function used to get PiWorker general statistics.
-func GetStatistics(tasks *data.UserData) (statistics *Statistic, err error) {
-	// tasks, err := data.ReadData()
-	// if err != nil {
-	// 	return nil, err
-	// }
+func UpdateRPiStats() error {
+	Current.Lock()
+	defer Current.Unlock()
 
-	activeTasks := len(*tasks.GetActiveTasks())
-	inactiveTasks := len(*tasks.GetInactiveTasks())
-	completedTasks := len(*tasks.GetCompletedTasks())
-	onExecutionTasks := len(*tasks.GetOnExecutionTasks())
-
-	// Raspberry
-	rTemperature, err := getRaspberryTemperature()
+	// CPU stats
+	cpuL, err := cpu.Percent(0, false)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	rCPULoad, err := getRaspberryCPULoad()
+	Current.RaspberryStats.CPULoad = cpuL[0]
+
+	// Storage stats
+	d, err := disk.Usage("/")
 	if err != nil {
-		return nil, err
+		return err
 	}
-	rFreeStorage, err := getRaspberryFreeStorage()
+	Current.RaspberryStats.Storage.Total = d.Total
+	Current.RaspberryStats.Storage.Free = d.Free
+	Current.RaspberryStats.Storage.Used = d.Used
+	Current.RaspberryStats.Storage.UsedPercent = d.UsedPercent
+
+	// RAM stats
+	vms, err := mem.VirtualMemory()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	rRAMUsage, err := getRaspberryRAMUsage()
+	Current.RaspberryStats.RAM.Total = vms.Total
+	Current.RaspberryStats.RAM.Available = vms.Available
+	Current.RaspberryStats.RAM.Used = vms.Used
+
+	// Host stats
+	st, err := host.SensorsTemperatures()
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	return &Statistic{
-		ActiveTasks:          activeTasks,
-		InactiveTasks:        inactiveTasks,
-		OnExecutionTasks:     onExecutionTasks,
-		CompletedTasks:       completedTasks,
-		AverageExecutionTime: 0.0, //TODO
-		OperatingTime:        0,   //TODO
-		BackupLoopState:      data.BackupLoopState,
-
-		RaspberryStats: RaspberryStats{
-			Temperature: rTemperature,
-			CPULoad:     rCPULoad,
-			FreeStorage: rFreeStorage,
-			RAMUsage:    rRAMUsage,
-		},
-	}, nil
-}
-
-func getRaspberryTemperature() (temperature float64, err error) {
-	rgx := regexp.MustCompile(`(?m)^\w+=([0-9]+\.[0-9]).+$`)
-
-	cmd := exec.Command("vcgencmd", "measure_temp")
-	output, err := cmd.Output()
+	bt, err := host.BootTime()
 	if err != nil {
-		return 0.0, err
+		return err
 	}
-
-	match := rgx.FindStringSubmatch(string(output))
-	if match != nil {
-		temp, err := strconv.ParseFloat(match[1], 64)
-		if err != nil {
-			return 0.0, err
-		}
-		return temp, nil
-	}
-
-	return 0.0, ErrBadTemperatureParse
-}
-
-func getRaspberryCPULoad() (cpuload string, err error) {
-	filePath := "/proc/stat"
-	rgx := regexp.MustCompile(
-		`(?m)^cpu[^0-9]\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)\s*([0-9]+)[\s0-9]*`,
-	)
-
-	file, err := os.Open(filePath)
+	ut, err := host.Uptime()
 	if err != nil {
-		return "", err
+		return err
 	}
-	defer file.Close()
+	Current.RaspberryStats.Host.Temperatures = st
+	Current.RaspberryStats.Host.BootTime = bt
+	Current.RaspberryStats.Host.UpTime = ut
 
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		return "", err
-	}
-
-	match := rgx.FindStringSubmatch(string(content))
-	if len(match) > 1 {
-		values := make([]int64, 0)
-		for index, m := range match {
-			if index == 0 {
-				continue
-			} // Skip the line matched
-			val, err := strconv.ParseInt(m, 10, 64)
-			if err != nil {
-				return "", err
-			}
-			values = append(values, val)
-		}
-		result := (values[3] * 100) / (values[0] + values[1] + values[2] +
-			values[3] + values[4] + values[5] + values[6])
-
-		return string(result) + "%", nil
-	}
-
-	return "", ErrBadCPULoadParse
-}
-
-func getRaspberryFreeStorage() (freestorage string, err error) {
-	rgx := regexp.MustCompile(
-		`(?m)^/dev/root\s+(\w+,?\w*)\s+(\w+,?\w*)\s+(\w+,?\w*).+$`,
-	)
-
-	cmd := exec.Command("df", "-h")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	match := rgx.FindStringSubmatch(string(output))
-	if match != nil {
-		freestorage := match[3]
-		// totalstorage := match[1] -> can be used
-		// usedstorage := match[2] -> can be used
-
-		return freestorage, nil
-	}
-
-	return "", ErrBadFreeStorageParse
-}
-
-func getRaspberryRAMUsage() (ramusage string, err error) {
-	rgx := regexp.MustCompile(
-		`(?m)^Mem:\s+(\w+,?\w*)\s+(\w+,?\w*).+`,
-	)
-
-	cmd := exec.Command("free", "-h")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
-
-	match := rgx.FindStringSubmatch(string(output))
-	if match != nil {
-		ramusage := match[2]
-		// totalmemory := match[1] -> can be used
-
-		return ramusage, nil
-	}
-
-	return "", ErrBadRAMUsageParse
+	return nil
 }
