22c1de906daedff2f48d03d111a9581b6993e21d
Pegasus <martingda@protonmail.com>
2020-03-10 11:14:02 -0300

    Removed `TasksStats` struct
    Because will be replaced by the struct `Statistic`, from the stats
    package.

--- core/engine/models.go
+++ core/engine/models.go
@@ -1,16 +1,1 @@
 package engine
-
-import (
-	"time"
-	"sync"
-)
-
-// TasksStats is the struct used to parse the statistics related with the execution of the tasks.
-type TasksStats struct{
-	Active uint16
-	OnExec uint8
-	Inactive uint16
-	AvgExecTime time.Duration
-	sync.RWMutex
-	observations uint32
-}
