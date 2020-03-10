73585b562874f34da43e6a106fe0bc98a7c937d5
Pegasus <martingda@protonmail.com>
2020-03-10 11:17:13 -0300

    Added global variable `Current`

--- core/stats/variables.go
+++ core/stats/variables.go
@@ -7,3 +7,7 @@ const (
 	// StatisticsPath is the path where the stats will be saved.
 	StatisticsPath = "./statistics/"
 )
+
+// Current is the variable that holds the different statistics of the tasks's execution and the 
+// Raspberry Pi running PiWorker.
+var Current Statistic
