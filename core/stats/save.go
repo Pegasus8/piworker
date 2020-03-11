498039a11419b567b2b0e8fbf60ff93f8149575b
Pegasus <martingda@protonmail.com>
2020-03-11 12:05:53 -0300

    Implemented fn to read the stats from the db

--- core/stats/save.go
+++ core/stats/save.go
@@ -141,37 +141,92 @@ func StoreStats(db *sql.DB, ts *TasksStats, rs *RaspberryStats) error {
 	return nil
 }
 
-/*
-// ReadRaspberryStatistics is the function used to read the raspberry's
-// statistics stored in the sqlite3 database.
-func ReadRaspberryStatistics(db *sql.DB) ([]RaspberryStats, error) {
-	sqlStatement := `
-	SELECT * FROM RaspberryStats
-	ORDER BY datetime(Timestamp) DESC
+
+// ReadStats reads the statistics stored in the stats database on a specific period of time.
+func ReadStats(db *sql.DB, from, to string) (*[]TasksStats, *[]RaspberryStats, error) {
+	// sqlStatement := `
+	// SELECT * FROM RaspberryStats
+	// ORDER BY datetime(Timestamp) DESC
+	// `
+	sqlStatement1 := `
+		SELECT * FROM TasksStats
+		WHERE date(Timestamp) BETWEEN ? AND ?;
 	`
+	sqlStatement2 := `
+		SELECT * FROM RaspberryStats
+		WHERE date(Timestamp) BETWEEN ? AND ?;
+	`
+	var ts []TasksStats
+	var rs []RaspberryStats
 
-	rows, err := db.Query(sqlStatement)
+	tsRows, err := db.Query(sqlStatement1, from, to)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	defer rows.Close()
+	defer tsRows.Close()
 
-	var result []RaspberryStats
-	for rows.Next() {
-		var item RaspberryStats
-		err = rows.Scan(
-			&item.Temperature,
-			&item.CPULoad,
-			&item.FreeStorage,
-			&item.RAMUsage,
+	
+	for tsRows.Next() {
+		var item TasksStats
+		var avg float64
+
+		err = tsRows.Scan(
+			&item.ActiveTasks,
+			&item.InactiveTasks,
+			&item.OnExecutionTasks,
+			&item.FailedTasks,
+			&avg,
 			&item.Timestamp,
 		)
 		if err != nil {
-			return nil, err
+			return &ts, &rs, err
 		}
 
-		result = append(result, item)
+		item.AverageExecutionTime = time.Duration(avg)
+
+		ts = append(ts, item)
 	}
 
-	return result, nil
-}*/
+	rsRows, err := db.Query(sqlStatement2, from, to)
+	if err != nil {
+		return &ts, &rs, err
+	}
+	defer rsRows.Close()
+
+	for rsRows.Next() {
+		var item RaspberryStats
+		var host string
+		var storage string
+		var ram string
+
+		err = rsRows.Scan(
+			&host,
+			&item.CPULoad,
+			&storage,
+			&ram,
+			&item.Timestamp,
+		)
+		if err != nil {
+			return &ts, &rs, err
+		}
+
+		err = json.Unmarshal([]byte(host), &item.Host)
+		if err != nil {
+			return &ts, &rs, err
+		}
+
+		err = json.Unmarshal([]byte(storage), &item.Storage)
+		if err != nil {
+			return &ts, &rs, err
+		}
+
+		err = json.Unmarshal([]byte(ram), &item.RAM)
+		if err != nil {
+			return &ts, &rs, err
+		}
+
+		rs = append(rs, item)
+	}
+
+	return &ts, &rs, nil
+}
