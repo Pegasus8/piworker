05e3d4376f94f396af42da151c5ff6ed2a4eeab7
Pegasus <martingda@protonmail.com>
2020-03-26 19:59:55 -0300

    Added `Chart` component for `statistics-detailed` view

---
+++ webui/frontend/src/components/statistics-detailed/Chart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: {
+    chartData: {
+      required: true,
+      type: Object
+    },
+    options: {
+      required: true,
+      type: Object
+    }
+  },
+  watch: {
+    options () {
+      this.renderChart(this.chartData, this.options)
+    },
+    chartData () {
+      this.renderChart(this.chartData, this.options)
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
