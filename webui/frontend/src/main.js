a788764a2d0bbc343c4ac91b3571846ce1caff8c
Pegasus <martingda@protonmail.com>
2020-03-30 12:42:08 -0300

    Usage of custom WebSocket plugin

--- webui/frontend/src/main.js
+++ webui/frontend/src/main.js
@@ -4,12 +4,17 @@ import router from './router'
 import store from './store/store'
 import axios from 'axios'
 import UUID from 'vue-uuid'
+import VueWebSocket from './services/websocket'
 
 import vuetify from './plugins/vuetify'
 require('typeface-roboto')
 
 Vue.config.productionTip = false
 Vue.use(UUID)
+Vue.use(VueWebSocket, {
+  url: 'ws://' + location.host + '/ws',
+  store: store
+})
 
 // Use the protocol used to access the WebUI (HTTP/S)
 axios.defaults.baseURL = `${location.protocol}//${location.host}`
