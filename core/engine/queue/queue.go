32aae64150cc34ea8ea8d9756ec4ed7f19c94368
Pegasus <martingda@protonmail.com>
2020-04-09 11:50:10 -0300

    Implemented actions queue

---
+++ core/engine/queue/queue.go
@@ -0,0 +1,57 @@
+package queue
+
+import (
+	"github.com/Pegasus8/piworker/core/data"
+	"github.com/Pegasus8/piworker/core/elements/actions"
+
+	"github.com/rs/zerolog/log"
+)
+
+// NewQueue initializes the pool of actions execution.
+func NewQueue() *Queue {
+	const maxWorkers = 8
+	queue := &Queue{
+		q: make(chan Job),
+	}
+
+	for i := 1; i <= maxWorkers; i++ {
+		go worker(i, queue.q)
+	}
+
+	return queue
+}
+
+// AddJob adds a new job to be processed by the workers.
+func (q *Queue) AddJob(taskID string, action actions.Action, userAction *data.UserAction, previousCR actions.ChainedResult) (result chan ExecResult) {
+	j := Job{
+		TaskID:     taskID,
+		Action:     action,
+		UserAction: userAction,
+		PreviousCR: previousCR,
+		OutputChan: make(chan ExecResult),
+	}
+
+	q.q <- j
+
+	return j.OutputChan
+}
+
+func worker(id int, inputs chan Job) {
+	log.Info().Int("workerID", id).Msg("Starting worker")
+
+	for {
+		job := <-inputs
+		log.Info().Int("workerID", id).Str("taskID", job.TaskID).Msg("New job received!")
+
+		// Action execution
+		r, cr, err := job.Action.Run(&job.PreviousCR, job.UserAction, job.TaskID)
+		execResult := ExecResult{
+			Successful:  r,
+			RetournedCR: *cr,
+			Err:         err,
+		}
+
+		log.Info().Int("workerID", id).Str("taskID", job.TaskID).Msg("Job executed!, reporting result...")
+		job.OutputChan <- execResult
+	}
+}
