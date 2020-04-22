753f435154e06bdb937af0eb936ee2a3ee92a66b
Pegasus <martingda@protonmail.com>
2020-04-22 11:57:06 -0300

    Improvements on the action SetLocalVariable
    Changes:
    - Improved code readability.
    - Implemented handling of chained results.

--- core/elements/actions/models/setlv/setlv.go
+++ core/elements/actions/models/setlv/setlv.go
@@ -2,51 +2,47 @@ package setlv
 
 import (
 	"errors"
+	"strings"
+
 	"github.com/Pegasus8/piworker/core/data"
 	"github.com/Pegasus8/piworker/core/elements/actions/shared"
 	"github.com/Pegasus8/piworker/core/types"
 	"github.com/Pegasus8/piworker/core/uservariables"
-	"strings"
 )
 
-const (
-	// Action
-	actionID = "A5"
+const actionID = "A5"
 
-	// Args
-	arg1ID    = actionID + "-1"
-	arg2ID = actionID + "-2"
-)
+var actionArgs = []shared.Arg{
+	shared.Arg{
+		ID:   actionID + "-1",
+		Name: "Name",
+		Description: "The name of the variable. Must be lowercase, without spaces or special characters. " +
+			"The unique special character allowed is the underscore ('_'). Remind that local variables are only " +
+			"valid on the task where are created. If you want share a variable between tasks use a global variable" +
+			" instead. Example of variable: some_random_local_var",
+		ContentType: types.Text,
+	},
+	shared.Arg{
+		ID:   actionID + "-2",
+		Name: "Variable content",
+		Description: "The content of the variable. Optionally can be: a result of a previous action, " +
+			"another variable or static content (setted by you).",
+		ContentType: types.Any,
+	},
+}
 
 // SetLocalVariable - Action
 var SetLocalVariable = shared.Action{
-	ID:          actionID,
-	Name:        "Set Local Variable",
-	Description: "Sets the content of a local variable. If the variable does not exist, it will be created.",
-	Run:         setLocalVariableAction,
-	Args: []shared.Arg{
-		shared.Arg{
-			ID:   arg1ID,
-			Name: "Name",
-			Description: "The name of the variable. Must be lowercase, without spaces or special characters. " +
-				"The unique special character allowed is the underscore ('_'). Remind that local variables are only " +
-				"valid on the task where are created. If you want share a variable between tasks use a global variable" +
-				" instead. Example of variable: some_random_local_var",
-			ContentType: types.Text,
-		},
-		shared.Arg{
-			ID:   arg2ID,
-			Name: "Variable content",
-			Description: "The content of the variable. Optionally can be: a result of a previous action, " +
-				"another variable or static content (setted by you).",
-			ContentType: types.Any,
-		},
-	},
+	ID:                             actionID,
+	Name:                           "Set Local Variable",
+	Description:                    "Sets the content of a local variable. If the variable does not exist, it will be created.",
+	Run:                            action,
+	Args:                           actionArgs,
 	ReturnedChainResultDescription: "The content setted to the variable.",
 	ReturnedChainResultType:        types.Any,
 }
 
-func setLocalVariableAction(previousResult *shared.ChainedResult, parentAction *data.UserAction, parentTaskID string) (result bool, chainedResult *shared.ChainedResult, err error) {
+func action(previousResult *shared.ChainedResult, parentAction *data.UserAction, parentTaskID string) (result bool, chainedResult *shared.ChainedResult, err error) {
 	var args *[]data.UserArg
 
 	// The name of the variable
@@ -56,13 +52,16 @@ func setLocalVariableAction(previousResult *shared.ChainedResult, parentAction *
 
 	args = &parentAction.Args
 
+	err = shared.HandleCR(parentAction, actionArgs, previousResult)
+	if err != nil {
+		return false, &shared.ChainedResult{}, err
+	}
+
 	for _, arg := range *args {
 		switch arg.ID {
-		case arg1ID:
-			{
-				variableName = strings.TrimSpace(arg.Content)
-			}
-		case arg2ID:
+		case actionArgs[0].ID:
+			variableName = strings.TrimSpace(arg.Content)
+		case actionArgs[1].ID:
 			variableContent = arg.Content
 		default:
 			{
