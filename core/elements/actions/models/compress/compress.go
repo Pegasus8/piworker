031034c4a41e9dedb1b6990799428af05d7b572e
Pegasus <martingda@protonmail.com>
2020-04-24 17:01:15 -0300

    Multiple changes on the action CompressFilesOfDir
    Changes:
    - Removed previous implementation and re-implemented it from zero (thanks to https://stackoverflow.com/questions/37869793/how-do-i-zip-a-directory-containing-sub-directories-or-files-in-golang).
    - Improved code readability.
    - Handling of chained results.

--- core/elements/actions/models/compress/compress.go
+++ core/elements/actions/models/compress/compress.go
@@ -1,137 +1,88 @@
 package compress
 
 import (
-	"compress/gzip"
 	"errors"
-	"io/ioutil"
-	"os"
 	"path/filepath"
-	
+	"strings"
+
 	"github.com/Pegasus8/piworker/core/data"
 	"github.com/Pegasus8/piworker/core/elements/actions/shared"
 	"github.com/Pegasus8/piworker/core/types"
-	"github.com/rs/zerolog/log"
 )
 
-// ID's
-const (
-	// Action
-	actionID = "A2"
+const actionID = "A2"
 
-	// Args
-	arg1ID = actionID + "-1"
-	arg2ID    = actionID + "-2"
-)
+var actionArgs = []shared.Arg{
+	shared.Arg{
+		ID:   actionID + "-1",
+		Name: "Directory/file target",
+		Description: "The directory where the files to compress are located." +
+			" Example: '/home/pegasus8/Images/'",
+		ContentType: types.Path,
+	},
+	shared.Arg{
+		ID:   actionID + "-2",
+		Name: "Directory where to store the compressed file",
+		Description: "Directory where save the compressed file, if not exists " +
+			"it will be created. Example: '/home/'",
+		ContentType: types.Path,
+	},
+	shared.Arg{
+		ID:          actionID + "-3",
+		Name:        "Name of the output file",
+		Description: "The name of the zip file (without the extension). For example: 'my_files'",
+		ContentType: types.Text,
+	},
+}
 
 // CompressFilesOfDir - Action
 var CompressFilesOfDir = shared.Action{
-	ID:   actionID,
-	Name: "Compress Files of a Directory",
-	Description: "Compress the files of a directory in gzip format.\nNote: it won't " +
-		"compress subdirectories, just files.",
-	Run: compressFilesOfDir,
-	Args: []shared.Arg{
-		shared.Arg{
-			ID:   arg1ID,
-			Name: "Directory Target",
-			Description: "The directory where the files to compress are located." +
-				" Example: '/home/pegasus8/Images/'",
-			// Content: "",
-			ContentType: types.Path,
-		},
-		shared.Arg{
-			ID:   arg2ID,
-			Name: "Directory Where Save",
-			Description: "Directory where save the compressed file, if not exists " +
-				"it will be created. Example: '/home/'",
-			// Content: "",
-			ContentType: types.Path,
-		},
-	},
-	ReturnedChainResultDescription: "The path of the compressed file.",
+	ID:                             actionID,
+	Name:                           "Compress Files of a Directory",
+	Description:                    "",
+	Run:                            action,
+	Args:                           actionArgs,
+	ReturnedChainResultDescription: "The path to the compressed file.",
 	ReturnedChainResultType:        types.Path,
 }
 
-func compressFilesOfDir(previousResult *shared.ChainedResult, parentAction *data.UserAction, parentTaskID string) (result bool, chainedResult *shared.ChainedResult, err error) {
+func action(previousResult *shared.ChainedResult, parentAction *data.UserAction, parentTaskID string) (result bool, chainedResult *shared.ChainedResult, err error) {
 	var args *[]data.UserArg
 
-	// Directory of files
 	var targetDir string
-	// Output dir
 	var outputDir string
+	var outputFilename string
 
 	args = &parentAction.Args
 
+	err = shared.HandleCR(parentAction, actionArgs, previousResult)
+	if err != nil {
+		return false, &shared.ChainedResult{}, err
+	}
+
 	for _, arg := range *args {
 		switch arg.ID {
-		case arg1ID:
+		case actionArgs[0].ID:
 			targetDir = filepath.Clean(arg.Content)
-		case arg2ID:
+		case actionArgs[1].ID:
 			outputDir = filepath.Clean(arg.Content)
-
+		case actionArgs[2].ID:
+			outputFilename = strings.ReplaceAll(strings.ReplaceAll(arg.Content, " ", "_"), "/", "-")
 		default:
 			return false, &shared.ChainedResult{}, shared.ErrUnrecognizedArgID
 		}
 	}
 
-	if targetDir == "" || outputDir == "" {
-		return false, &shared.ChainedResult{}, errors.New("Error: targetDir or outputDir empty")
+	if targetDir == "" || outputDir == "" || outputFilename == "" {
+		return false, &shared.ChainedResult{}, errors.New("Error: targetDir, outputDir or outputFilename empty")
 	}
 
-	log.Info().Str("taskID", parentTaskID).Msgf("Creating the directory '%s' if it doesn't exists...", outputDir)
-	err = os.MkdirAll(outputDir, 0700)
-	if err != nil {
-		return false, &shared.ChainedResult{}, nil
-	}
+	outputDir = filepath.Join(outputDir, outputFilename+".zip")
 
-	log.Info().Str("taskID", parentTaskID).Msgf("Getting the files of the directory '%s'", targetDir)
-	files, err := ioutil.ReadDir(targetDir)
+	err = zipWriter(targetDir, outputDir)
 	if err != nil {
 		return false, &shared.ChainedResult{}, err
 	}
-	log.Info().Str("taskID", parentTaskID).Msg("Files obtained")
-
-	for _, file := range files {
-		if file.IsDir() {
-			log.Warn().Str("taskID", parentTaskID).Msgf("Skipping '%s' because it isn't a file", file.Name())
-			continue
-		}
-		log.Info().Str("taskID", parentTaskID).Msgf("Starting the compression of the file '%s'...", file.Name())
-
-		openedFile, err := os.Open(
-			filepath.Join(targetDir, file.Name()),
-		)
-		if err != nil {
-			return false, &shared.ChainedResult{}, err
-		}
-		defer openedFile.Close()
-
-		content, err := ioutil.ReadAll(openedFile)
-		if err != nil {
-			return false, &shared.ChainedResult{}, err
-		}
-
-		newFilename := file.Name() + ".gz"
-		newPath := filepath.Join(outputDir, newFilename)
-
-		outputFile, err := os.Create(newPath)
-		if err != nil {
-			return false, &shared.ChainedResult{}, err
-		}
-
-		gzipWriter := gzip.NewWriter(outputFile)
-		defer gzipWriter.Close()
-
-		_, err = gzipWriter.Write(content)
-		if err != nil {
-			return false, &shared.ChainedResult{}, err
-		}
-
-		log.Info().Str("taskID", parentTaskID).Msgf("'%s' compressed by the action CompressFilesOfDir", newFilename)
-
-	}
-
-	log.Info().Str("taskID", parentTaskID).Msgf("Files compression finished into directory '%s'", outputDir)
 
 	return true, &shared.ChainedResult{Result: outputDir, ResultType: types.Path}, nil
 }
---
+++ core/elements/actions/models/compress/zip.go
@@ -0,0 +1,65 @@
+package compress
+
+import (
+	"archive/zip"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func zipWriter(targetPath, outputPath string) error {
+	target := filepath.Clean(targetPath)
+	output := filepath.Clean(outputPath)
+
+	outputFile, err := os.Create(output)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	w := zip.NewWriter(outputFile)
+
+	err = addFiles(w, target, "")
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func addFiles(w *zip.Writer, basePath, baseInZip string) error {
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			content, err := ioutil.ReadFile(filepath.Join(basePath, file.Name()))
+			if err != nil {
+				return err
+			}
+
+			f, err := w.Create(baseInZip + file.Name())
+			if err != nil {
+				return err
+			}
+
+			_, err = f.Write(content)
+			if err != nil {
+				return err
+			}
+		} else {
+			// If it's a directory, execute the function recursively.
+			newBase := filepath.Join(basePath, file.Name())
+			addFiles(w, newBase, baseInZip+file.Name()+"/")
+		}
+	}
+
+	return nil
+}
