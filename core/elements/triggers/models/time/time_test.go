a6ec0a6cf22369ec3a7ad3da7f0592ad66721367
Pegasus <martingda@protonmail.com>
2020-05-07 15:58:48 -0300

    Added aditional test cases

--- core/elements/triggers/models/time/time_test.go
+++ core/elements/triggers/models/time/time_test.go
@@ -32,7 +32,49 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [1] -- Incorrect --
+		// [1] -- Correct --
+		// Problem: 		None.
+		// Expected result: Should return no errors and a false result.
+		[]data.UserArg{
+			data.UserArg{
+				ID:      ByTime.Args[0].ID,
+				Content: time.Now().Add(time.Hour * 24).Format("2006-01-02"),
+			},
+			data.UserArg{
+				ID:      ByTime.Args[1].ID,
+				Content: time.Now().Format("15:04"),
+			},
+		},
+
+		// [2] -- Correct --
+		// Problem: 		None.
+		// Expected result: Should return no errors and a false result.
+		[]data.UserArg{
+			data.UserArg{
+				ID:      ByTime.Args[0].ID,
+				Content: time.Now().Format("2006-01-02"),
+			},
+			data.UserArg{
+				ID:      ByTime.Args[1].ID,
+				Content: time.Now().Add(time.Minute * 2).Format("15:04"),
+			},
+		},
+
+		// [3] -- Correct --
+		// Problem: 		None.
+		// Expected result: Should return no errors and a false result.
+		[]data.UserArg{
+			data.UserArg{
+				ID:      ByTime.Args[0].ID,
+				Content: time.Now().Add(time.Hour * 24).Format("2006-01-02"),
+			},
+			data.UserArg{
+				ID:      ByTime.Args[1].ID,
+				Content: time.Now().Add(time.Minute * 2).Format("15:04"),
+			},
+		},
+
+		// [4] -- Incorrect --
 		// Problem: 		The first arg ("Date") is incorrectly formatted.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -46,7 +88,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [2] -- Incorrect --
+		// [5] -- Incorrect --
 		// Problem: 		The second arg ("Hour") is incorrectly formatted.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -60,7 +102,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [3] -- Incorrect --
+		// [6] -- Incorrect --
 		// Problem: 		Both args are incorrectly formatted.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -74,7 +116,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [4] -- Incorrect --
+		// [7] -- Incorrect --
 		// Problem: 		ID of an arg (0) is empty.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -88,7 +130,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [5] -- Incorrect --
+		// [8] -- Incorrect --
 		// Problem: 		ID of an arg (1) is incorrect.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -102,7 +144,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [6] -- Incorrect --
+		// [9] -- Incorrect --
 		// Problem: 		There are only one argument (should be two).
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -112,7 +154,7 @@ func TestByTime(t *testing.T) {
 			},
 		},
 
-		// [7] -- Incorrect --
+		// [10] -- Incorrect --
 		// Problem: 		Content of an argument empty.
 		// Expected result: Should return an error and a false result.
 		[]data.UserArg{
@@ -127,15 +169,19 @@ func TestByTime(t *testing.T) {
 		},
 	}
 
-	r, err := ByTime.Run(&args[0], taskID)
-	assert.Equal(true, r, "the trigger must be executed correctly")
-	if r {
-		assert.NoError(err, "if the result of the trigger is true, there should be no errors")
+	for i, arg := range args[:4] {
+		r, err := ByTime.Run(&arg, taskID)
+		if i == 0 {
+			assert.Equalf(true, r, "[arg %d] the trigger must be executed correctly", i)
+		} else {
+			assert.Equalf(false, r, "[arg %d] the trigger must be executed correctly", i)
+		}
+		assert.NoErrorf(err, "[arg %d] there should be no errors", i)
 	}
 
-	for i, arg := range args[1:] {
+	for i, arg := range args[4:] {
 		r, err := ByTime.Run(&arg, taskID)
-		assert.Equal(false, r, "the trigger must return a false result if at least one argument is incorrect")
-		assert.Errorf(err, "the usage of the argument %d must return an error", i)
+		assert.Equalf(false, r, "[arg %d]the trigger must return a false result if at least one argument is incorrect", i)
+		assert.Errorf(err, "[arg %d] an error must be returned", i)
 	}
 }
