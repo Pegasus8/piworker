73b6c4016c49bad77e3979eabc5278d1dcdf96bb
Martín Díaz <martingda@protonmail.com>
2020-06-13 20:37:12 -0300

    Modified some returned errors
    If the file does not exist, `ErrNoConfigFileDetected` will be returned.
    If the file has an incorrect format (corrupted), `ErrConfigFileCorrupted` will be returned.
    Also, handled a possible error on file close.

--- core/configs/read.go
+++ core/configs/read.go
@@ -2,6 +2,7 @@ package configs
 
 import (
 	"encoding/json"
+	"github.com/rs/zerolog/log"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -10,16 +11,24 @@ import (
 // ReadFromFile is a method used to read the configs file and parse the content into
 // the `Configs` struct.
 func ReadFromFile() error {
-	fullpath := filepath.Join(Path, Filename)
+	f := filepath.Join(Path, Filename)
 
 	mutex.Lock()
 	defer mutex.Unlock()
 
-	jsonData, err := os.Open(fullpath)
+	jsonData, err := os.Open(f)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrNoConfigFileDetected
+		}
 		return err
 	}
-	defer jsonData.Close()
+	defer func() {
+		err := jsonData.Close()
+		if err != nil {
+			log.Error().Err(err).Msg("Error when trying to close the file of configs after reading it")
+		}
+	}()
 
 	byteContent, err := ioutil.ReadAll(jsonData)
 	if err != nil {
@@ -29,7 +38,7 @@ func ReadFromFile() error {
 	var cfg Configs
 	err = json.Unmarshal(byteContent, &cfg)
 	if err != nil {
-		return err
+		return ErrConfigFileCorrupted
 	}
 
 	// Update configs variable
