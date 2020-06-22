4cdba3b41c123f7e1ab27f6b38f134de1c03c9c3
Martín Díaz <martingda@protonmail.com>
2020-06-22 10:43:15 -0300

    Removed unnecessary log statements

--- core/data/delete.go
+++ core/data/delete.go
@@ -1,23 +1,16 @@
 package data
 
-import (
-	"github.com/rs/zerolog/log"
-)
-
 // DeleteTask is a function used to delete a specific task from the table 'Tasks', on the SQLite3 database.
 func DeleteTask(ID string) error {
 	sqlStatement := `
 		DELETE FROM Tasks
 		WHERE ID = ?;
 	`
-	log.Info().Str("taskID", ID).Msg("Deleting task...")
 
 	_, err := DB.Exec(sqlStatement, ID)
 	if err != nil {
 		return err
 	}
-
-	log.Info().Str("taskID", ID).Msg("Task deleted successfully")
 
 	event := Event{
 		Type:   Deleted,
