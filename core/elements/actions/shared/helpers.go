123b6ccb5599968e01ae1d21ff20b6fdd2d832b4
Martín Díaz <martingda@protonmail.com>
2020-07-06 12:22:28 -0300

    Added check of compatibility between the coming chained result and the current action

--- core/elements/actions/shared/helpers.go
+++ core/elements/actions/shared/helpers.go
@@ -7,15 +7,22 @@ import (
 // HandleCR checks if the usage of the `ChainedResult` it's enabled and, if it is,
 // the content of the argument chained will be replaced with the content of the `ChainedResult`.
 func HandleCR(userAction *data.UserAction, actionArgs []Arg, cr *ChainedResult) error {
+	if userAction.Order == 0 {
+		return nil
+	}
+
 	if !userAction.Chained {
 		return nil
 	}
+
 	if cr.Result == "" {
 		return ErrEmptyCRResult
 	}
+
 	if cr.ResultType == "" {
 		return ErrEmptyCRResultType
 	}
+
 	if userAction.ArgumentToReplaceByCR == "" {
 		return ErrEmptyArgToReplace
 	}
@@ -27,7 +34,7 @@ func HandleCR(userAction *data.UserAction, actionArgs []Arg, cr *ChainedResult) 
 			for _, pwArg := range actionArgs {
 				if arg.ID == pwArg.ID {
 					// Only use the content of the CR if the type is compatible.
-					if pwArg.ContentType == cr.ResultType {
+					if pwArg.ContentType == cr.ResultType || pwArg.ContentType.CompatWith(cr.ResultType) {
 						userAction.Args[i].Content = cr.Result
 					} else {
 						return ErrCRTypeDiffers
