255fd1c9afa9cb82121888cd648d6f0840cb59e9
Pegasus <martingda@protonmail.com>
2021-01-20 17:53:14 -0300

    Added methods to get/set user variables from the database (with tests)

---
+++ core/data/uservariables.go
@@ -0,0 +1,132 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Pegasus8/piworker/core/uservariables"
+	"github.com/rs/zerolog/log"
+)
+
+// SetULV sets the content of a user local variable stored in the database. If the variable doesn't exist, it will be
+// created with the given content.
+func (db *DatabaseInstance) SetULV(v *uservariables.LocalVariable) error {
+	exists, err := ulvExists(db, v.Name)
+	if err != nil {
+		return err
+	}
+
+	r, err := func() (sql.Result, error) {
+		var q string
+		if exists {
+			q = "UPDATE variables_local SET name = ?, content = ?, type = ?, parent_task_id = ? WHERE name = ?;"
+			return db.instance.Exec(q, v.Name, v.Content, v.Type, v.ParentTaskID, v.Name)
+		} else {
+			q = `INSERT INTO variables_local(name, content, type, parent_task_id) values (?, ?, ?, ?);`
+			return db.instance.Exec(q, v.Name, v.Content, v.Type, v.ParentTaskID)
+		}
+	}()
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("the variable with name '%s' was not inserted in the database", v.Name)
+	}
+
+	return nil
+}
+
+// GetULV returns the content of a user local variable stored in the database. If the requested variable doesn't exist,
+// an error will be returned.
+func (db *DatabaseInstance) GetULV(name string) (*uservariables.LocalVariable, error) {
+	// TODO
+	return nil, nil // TODO Change this
+}
+
+// SetUGV sets the content of a user global variable stored in the database. If the variable doesn't exist, it will be
+// created with the given content.
+func (db *DatabaseInstance) SetUGV(v *uservariables.GlobalVariable) error {
+	exists, err := ugvExists(db, v.Name)
+	if err != nil {
+		return err
+	}
+
+	r, err := func() (sql.Result, error) {
+		var q string
+		if exists {
+			q = "UPDATE variables_global SET name = ?, content = ?, type = ? WHERE name = ?;"
+			return db.instance.Exec(q, v.Name, v.Content, v.Type, v.Name)
+		} else {
+			q = `INSERT INTO variables_global(name, content, type) values (?, ?, ?);`
+			return db.instance.Exec(q, v.Name, v.Content, v.Type)
+		}
+	}()
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return fmt.Errorf("the variable with name '%s' was not inserted in the database", v.Name)
+	}
+
+	return nil
+}
+
+// GetUGV returns the content of a user global variable stored in the database. If the requested variable doesn't exist,
+// an error will be returned.
+func (db *DatabaseInstance) GetUGV(name string) (*uservariables.GlobalVariable, error) {
+	// TODO
+	return nil, nil // TODO Change this
+}
+
+func ulvExists(db *DatabaseInstance, name string) (bool, error) {
+	i := db.GetSQLInstance()
+
+	q := "SELECT name FROM variables_local WHERE name = ?;"
+
+	row, err := i.Query(q, name)
+	if err != nil {
+		return false, err
+	}
+
+	defer func() {
+		err := row.Close()
+		if err != nil {
+			log.Err(err).Msg("Error when trying to close rows")
+		}
+	}()
+
+	return row.Next(), nil
+}
+
+func ugvExists(db *DatabaseInstance, name string) (bool, error) {
+	i := db.GetSQLInstance()
+
+	q := "SELECT name FROM variables_global WHERE name = ?;"
+
+	row, err := i.Query(q, name)
+	if err != nil {
+		return false, err
+	}
+
+	defer func() {
+		err := row.Close()
+		if err != nil {
+			log.Err(err).Msg("Error when trying to close rows")
+		}
+	}()
+
+	return row.Next(), nil
+}
---
+++ core/data/uservariables_test.go
@@ -0,0 +1,191 @@
+package data
+
+import (
+	"github.com/Pegasus8/piworker/core/types"
+	"github.com/Pegasus8/piworker/core/uservariables"
+	assert2 "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"os"
+	"testing"
+)
+
+type DBUserVariablesTestSuite struct {
+	TestDir        string
+	TestFilename   string
+	TestDBInstance *DatabaseInstance
+	GlobalVars     []uservariables.GlobalVariable
+	LocalVars      []uservariables.LocalVariable
+
+	suite.Suite
+}
+
+func (s *DBUserVariablesTestSuite) SetupTest() {
+	s.TestDir = "./test_db_uv"
+	s.TestFilename = "uv.db"
+
+	err := os.Mkdir(s.TestDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := NewDB(s.TestDir, s.TestFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	s.TestDBInstance = db
+
+	i := s.TestDBInstance.GetSQLInstance()
+
+	s.LocalVars = []uservariables.LocalVariable{
+		{
+			Name:         "variable_1",
+			Content:      "10",
+			Type:         types.Int,
+			ParentTaskID: "abc1",
+		},
+		{
+			Name:         "variable_2",
+			Content:      "Hello world!",
+			Type:         types.Text,
+			ParentTaskID: "abc1",
+		},
+	}
+
+	s.GlobalVars = []uservariables.GlobalVariable{
+		{
+			Name:    "VARIABLE_1",
+			Content: "0.120",
+			Type:    types.Float,
+		},
+		{
+			Name:    "VARIABLE_2",
+			Content: "11/01/2021",
+			Type:    types.Date,
+		},
+	}
+
+	for _, v := range s.LocalVars {
+		q := `INSERT INTO variables_local(name, content, type, parent_task_id) values (?, ?, ?, ?);`
+
+		result, err := i.Exec(q, v.Name, v.Content, v.Type, v.ParentTaskID)
+		if err != nil {
+			panic(err)
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			panic(err)
+		}
+
+		if rowsAffected == 0 {
+			assert2.FailNow(s.T(), "The local variable '%+v' was not inserted in the database", v)
+		}
+	}
+
+	for _, v := range s.GlobalVars {
+		q := `INSERT INTO variables_global(name, content, type) values (?, ?, ?);`
+
+		result, err := i.Exec(q, v.Name, v.Content, v.Type)
+		if err != nil {
+			panic(err)
+		}
+
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			panic(err)
+		}
+
+		if rowsAffected == 0 {
+			assert2.FailNow(s.T(), "The global variable '%+v' was not inserted in the database", v)
+		}
+	}
+}
+
+func (s *DBUserVariablesTestSuite) BeforeTest(_, _ string) {}
+
+func (s *DBUserVariablesTestSuite) TestSetULV() {
+	assert := assert2.New(s.T())
+
+	v := s.LocalVars[0]
+	v.Content = "11"
+
+	// Try to overwrite an already existent variable.
+	err := s.TestDBInstance.SetULV(&v)
+
+	assert.NoError(err, "The user local variable should be set without errors")
+
+	// Try to add a third variable.
+	v.Name = "variable_3"
+
+	err = s.TestDBInstance.SetULV(&v)
+
+	assert.NoError(err, "The user local variable should be set without errors")
+}
+
+/*
+func (s *DBUserVariablesTestSuite) TestGetULV() {
+	assert := assert2.New(s.T())
+
+	v, err := s.TestDBInstance.GetULV(s.LocalVars[1].Name)
+
+	assert.NoError(err, "The variable should be returned without an error")
+	if assert.NotNil(v, "The returned variable should not be nil") {
+		assert.Equal(s.LocalVars[1], *v, "The returned variable should be the same as the inserted one")
+	}
+
+	v, err = s.TestDBInstance.GetULV("non_existent_variable")
+
+	assert.Error(err, "If the requested variable does not exist, an error should be returned")
+	assert.Nil(v, "If the requested variable does not exist, the returned variable should be nil")
+}
+*/
+func (s *DBUserVariablesTestSuite) TestSetUGV() {
+	assert := assert2.New(s.T())
+
+	v := s.GlobalVars[0]
+	v.Content = "0.121"
+
+	// Try to overwrite an already existent variable.
+	err := s.TestDBInstance.SetUGV(&v)
+
+	assert.NoError(err, "The user global variable should be set without errors")
+
+	// Try to add a third variable.
+	v.Name = "variable_3"
+
+	err = s.TestDBInstance.SetUGV(&v)
+
+	assert.NoError(err, "The user global variable should be set without errors")
+}
+
+/*
+func (s *DBUserVariablesTestSuite) TestGetUGV() {
+	assert := assert2.New(s.T())
+
+	v, err := s.TestDBInstance.GetUGV(s.GlobalVars[1].Name)
+
+	assert.NoError(err, "The variable should be returned without an error")
+	if assert.NotNil(v, "The returned variable should not be nil") {
+		assert.Equal(s.GlobalVars[1], *v, "The returned variable should be the same as the inserted one")
+	}
+
+	v, err = s.TestDBInstance.GetUGV("NON_EXISTENT_VARIABLE")
+
+	assert.Error(err, "If the requested variable does not exist, an error should be returned")
+	assert.Nil(v, "If the requested variable does not exist, the returned variable should be nil")
+}
+*/
+func (s *DBUserVariablesTestSuite) AfterTest(_, _ string) {}
+
+func (s *DBUserVariablesTestSuite) TearDownTest() {
+	_ = s.TestDBInstance.Close()
+	err := os.RemoveAll(s.TestDir)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestDBUserVariablesSuite(t *testing.T) {
+	suite.Run(t, new(DBUserVariablesTestSuite))
+}
